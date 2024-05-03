de8e3ae626e9f5babb9dfd33c93dc0dc0b9eadb6
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-03 21:17:25 +0200

    fix(components): update the navbar logo styles

--- src/components/navbar/logo.tsx
+++ src/components/navbar/logo.tsx
@@ -1,5 +1,5 @@
 export const Logo = () => (
-  <span className="text-transparent bg-clip-text text-2xl bg-gradient-to-b from-white to-slate-400 font-semibold">
+  <span className="text-transparent select-none bg-clip-text text-2xl bg-gradient-to-b from-white to-slate-400 font-semibold">
     Formizee.
   </span>
 );
