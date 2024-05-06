0f5d2293fa853eb78e5544db044481ffefa60989
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-06 13:38:34 +0200

    fix(app): update the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import {Navbar} from '@/components';
-import {Button, Input} from '@/components/ui';
+import {WaitlistForm} from '@/components/forms';
 
 const Home = () => {
   return (
@@ -7,10 +7,10 @@ const Home = () => {
       <Navbar />
       <header className="flex flex-col gap-y-8 items-start">
         <div className="flex flex-col gap-3">
-          <h1 className="text-transparent bg-clip-text leading-snug text-5xl bg-gradient-to-b from-white to-slate-400 font-semibold">
+          <h1 className="text-transparent bg-clip-text leading-[1.2] text-5xl bg-gradient-to-b from-white to-slate-400 font-semibold">
             Forms Backend
             <br />
-            built for developers.
+            Built for developers.
           </h1>
           <p className="w-full text-neutral-400 text-lg">
             The modern way to implement forms in your projects.
@@ -19,19 +19,7 @@ const Home = () => {
             <span className="text-white">Open Source</span>.
           </p>
         </div>
-        <form className="flex gap-2">
-          <Input placeholder="you@email.com" />
-          <Button className="gap-x-2">
-            <svg viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
-              <path
-                fillRule="evenodd"
-                d="M1 8a7 7 0 1 1 14 0A7 7 0 0 1 1 8Zm7 5.5a5.485 5.485 0 0 1-4.007-1.732l.28-.702a.402.402 0 0 1 .658-.135.804.804 0 0 0 1.138 0l.012-.012a.822.822 0 0 0 .154-.949l-.055-.11a.497.497 0 0 1 .134-.611L8.14 7.788a.57.57 0 0 0 .154-.7.57.57 0 0 1 .33-.796l.028-.01a1.788 1.788 0 0 0 1.13-1.13l.072-.214a.747.747 0 0 0-.18-.764L8.293 2.793A1 1 0 0 1 8.09 2.5 5.5 5.5 0 0 1 12.9 10.5h-.486a1 1 0 0 1-.707-.293l-.353-.353a1.207 1.207 0 0 0-1.708 0l-.531.531a1 1 0 0 1-.26.188l-.343.17a.927.927 0 0 0-.512.83v.177c0 .414.336.75.75.75a.75.75 0 0 1 .751.793c-.477.135-.98.207-1.501.207Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <span>Join The Waitlist</span>
-          </Button>
-        </form>
+        <WaitlistForm />
       </header>
     </main>
   );
