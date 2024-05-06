55fcb86cd9f2516cfe18cafbd641cef0a609d246
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-07 01:52:17 +0200

    fix(app): update the home page responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import {WaitlistForm} from '@/components/forms';
 
 const Home = () => {
   return (
-    <main className="flex bg-black min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex bg-black min-h-screen flex-col items-center lg:justify-center">
       <Navbar />
-      <header className="flex justify-between container">
-        <div className="flex flex-col gap-y-8 items-start">
-          <h1 className="text-transparent bg-clip-text leading-[1.2] text-5xl bg-gradient-to-b from-white to-slate-400 font-semibold">
+      <header className="flex w-full flex-col-reverse lg:flex-row lg:items-center justify-between">
+        <div className="ml-4 sm:ml-8 lg:ml-8 xl:ml-24 2xl:ml-64 flex flex-col gap-y-4 md:gap-y-8 items-start">
+          <h1 className="text-transparent bg-clip-text leading-[1.2] text-3xl sm:text-5xl bg-gradient-to-b from-white to-slate-400 font-semibold">
             Forms Backend
             <br />
             Built for developers.
           </h1>
-          <p className="w-full text-neutral-400 text-lg">
+          <p className="w-full text-neutral-400 text-md sm:text-lg">
             The modern way to implement forms in your projects.
             <br /> <span className="text-white">Smooth</span>,{' '}
             <span className="text-white">Effortless</span> and{' '}
