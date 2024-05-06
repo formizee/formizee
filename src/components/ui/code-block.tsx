247d7228ee944b60a5fbc18287e41ea9883aa89c
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-07 01:49:25 +0200

    fix(components): update the ui code block styles

--- src/components/ui/code-block.tsx
+++ src/components/ui/code-block.tsx
@@ -11,10 +11,12 @@ export const CodeBlock = (props: Props) => {
   return (
     <Highlight code={props.children} language={props.language}>
       {({tokens, getLineProps, getTokenProps}) => (
-        <pre style={{}} className="text-lg">
+        <pre style={{}} className="flex flex-col md:text-lg">
           {tokens.map((line, i) => (
             <div key={i} style={getLineProps({line}).style} className="my-1">
-              <span className="text-sm text-neutral-400 mr-3">{i + 1}</span>
+              <span className="text-sm select-none text-neutral-400 mr-3">
+                {i + 1}
+              </span>
               {line.map((token, key) => (
                 <span key={key} {...getTokenProps({token})} />
               ))}
