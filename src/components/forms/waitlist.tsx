054fe5567f565262a2511425046c63c10e440def
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-07 01:51:17 +0200

    fix(components): update waitlist form styles

--- src/components/forms/waitlist.tsx
+++ src/components/forms/waitlist.tsx
@@ -34,7 +34,7 @@ export const WaitlistForm = () => {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col gap-4 md:flex-row">
+        className="flex flex-col gap-4 sm:flex-row">
         <FormField
           control={form.control}
           name="email"
@@ -49,11 +49,8 @@ export const WaitlistForm = () => {
         />
         <Button className="gap-x-2">
           <svg viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
-            <path
-              fillRule="evenodd"
-              d="M1 8a7 7 0 1 1 14 0A7 7 0 0 1 1 8Zm7 5.5a5.485 5.485 0 0 1-4.007-1.732l.28-.702a.402.402 0 0 1 .658-.135.804.804 0 0 0 1.138 0l.012-.012a.822.822 0 0 0 .154-.949l-.055-.11a.497.497 0 0 1 .134-.611L8.14 7.788a.57.57 0 0 0 .154-.7.57.57 0 0 1 .33-.796l.028-.01a1.788 1.788 0 0 0 1.13-1.13l.072-.214a.747.747 0 0 0-.18-.764L8.293 2.793A1 1 0 0 1 8.09 2.5 5.5 5.5 0 0 1 12.9 10.5h-.486a1 1 0 0 1-.707-.293l-.353-.353a1.207 1.207 0 0 0-1.708 0l-.531.531a1 1 0 0 1-.26.188l-.343.17a.927.927 0 0 0-.512.83v.177c0 .414.336.75.75.75a.75.75 0 0 1 .751.793c-.477.135-.98.207-1.501.207Z"
-              clipRule="evenodd"
-            />
+            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
           <span>Join The Waitlist</span>
         </Button>
