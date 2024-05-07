e2284c86882dc7adffccb5a7e69c6267f08e1948
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-07 02:36:39 +0200

    fix(components): update ui code block theme

--- src/components/ui/code-block.tsx
+++ src/components/ui/code-block.tsx
@@ -1,4 +1,4 @@
-import {Highlight} from 'prism-react-renderer';
+import {Highlight, PrismTheme} from 'prism-react-renderer';
 
 export type LanguageType = 'html' | 'js' | 'jsx' | 'ts' | 'tsx';
 
@@ -7,9 +7,105 @@ interface Props {
   language: LanguageType;
 }
 
+const theme: PrismTheme = {
+  plain: {
+    cursor: '#fbbf24',
+    color: '#ffffff'
+  },
+  styles: [
+    {
+      types: ['comment', 'prolog', 'doctype', 'cdata'],
+      style: {
+        color: '#78716c'
+      }
+    },
+    {
+      types: ['namespace'],
+      style: {
+        opacity: 0.7
+      }
+    },
+    {
+      types: ['tag', 'operator', 'number'],
+      style: {
+        color: '#78716c'
+      }
+    },
+    {
+      types: ['property', 'function'],
+      style: {
+        color: '#9a86fd'
+      }
+    },
+    {
+      types: ['tag-id', 'selector', 'atrule-id'],
+      style: {
+        color: '#eeebff'
+      }
+    },
+    {
+      types: ['attr-name'],
+      style: {
+        color: '#a3a3a3'
+      }
+    },
+    {
+      types: [
+        'boolean',
+        'string',
+        'entity',
+        'url',
+        'attr-value',
+        'keyword',
+        'control',
+        'directive',
+        'unit',
+        'statement',
+        'regex',
+        'atrule',
+        'placeholder',
+        'variable'
+      ],
+      style: {
+        color: '#fbbf24'
+      }
+    },
+    {
+      types: ['deleted'],
+      style: {
+        textDecorationLine: 'line-through'
+      }
+    },
+    {
+      types: ['inserted'],
+      style: {
+        textDecorationLine: 'underline'
+      }
+    },
+    {
+      types: ['italic'],
+      style: {
+        fontStyle: 'italic'
+      }
+    },
+    {
+      types: ['important', 'bold'],
+      style: {
+        fontWeight: 'bold'
+      }
+    },
+    {
+      types: ['important'],
+      style: {
+        color: '#c4b9fe'
+      }
+    }
+  ]
+};
+
 export const CodeBlock = (props: Props) => {
   return (
-    <Highlight code={props.children} language={props.language}>
+    <Highlight theme={theme} code={props.children} language={props.language}>
       {({tokens, getLineProps, getTokenProps}) => (
         <pre style={{}} className="flex flex-col md:text-lg">
           {tokens.map((line, i) => (
