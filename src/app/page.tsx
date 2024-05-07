ebf4ed93f8dbecb6830cf9b9e9c85ea53644e884
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-07 13:54:35 +0200

    fix(app): update home page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import {Navbar, Spotlight} from '@/components';
+import { Navbar, Spotlight } from '@/components';
 import Cards from '@/components/cards';
-import {WaitlistForm} from '@/components/forms';
+import { WaitlistForm } from '@/components/forms';
 
 const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center overflow-x-clip bg-black lg:justify-center">
       <Spotlight className="absolute -left-10 -top-40 lg:-left-0" />
       <div className="pointer-events-none absolute inset-0 z-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-      <div className="grid-pattern pointer-events-none absolute inset-0" />
+      <div className="bg-grid-pattern pointer-events-none absolute inset-0" />
       <Navbar />
       <header className="z-10 flex w-full flex-col-reverse justify-between lg:flex-row lg:items-center">
         <div className="ml-4 flex flex-col items-start gap-y-4 sm:ml-8 md:gap-y-8 lg:ml-8 xl:ml-24 2xl:ml-64">
