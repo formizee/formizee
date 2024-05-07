2e6d7c99fb123f6b9d586830e4034c23b054493b
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-07 13:44:50 +0200

    feat(components): add the spotlight component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export {ThemeProvider} from './theme-provider';
+export {Spotlight} from './spotlight';
 export {BaseBody} from './base-body';
 export {Navbar} from './navbar';
 export {Logo} from './logo';
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import {fontFamily} from 'tailwindcss/defaultTheme';
 import type {Config} from 'tailwindcss';
+import {transform} from 'next/dist/build/swc';
 
 const config = {
   darkMode: ['class'],
@@ -31,11 +32,22 @@ const config = {
         'accordion-up': {
           from: {height: 'var(--radix-accordion-content-height)'},
           to: {height: '0'}
+        },
+        spotlight: {
+          from: {
+            opacity: '0',
+            transform: 'translate(-72%, -62%) scale(0.7)'
+          },
+          to: {
+            opacity: '100',
+            transform: 'translate(-50%,-40%) scale(1)'
+          }
         }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out'
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        spotlight: 'spotlight 1.5s ease-out forwards'
       }
     }
   },
