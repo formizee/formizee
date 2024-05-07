7dce8b93d15ded47071d325b50da38cb159b98fa
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-07 13:44:19 +0200

    fix(components): update the ui navbar to be transparent when the scroll is on top

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,39 @@
+'use client';
+
+import {useEffect, useState} from 'react';
+import {cn} from '@/lib/ui';
+
 import {Button} from '../ui';
 import Link from 'next/link';
 import Logo from './logo';
 
 export const Navbar = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, {passive: true});
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const background =
+    scrollPosition > 10
+      ? 'bg-black/70 border-b-neutral-800 backdrop-blur-md'
+      : 'bg-transparent border-b-transparent backdrop-blur-none';
+
   return (
-    <nav className="fixed top-0 z-50 flex h-14 w-full items-center justify-between bg-black px-4">
+    <nav
+      className={cn(
+        'fixed top-0 z-50 flex h-14 w-full items-center justify-between border-b px-4 transition-all duration-500',
+        background
+      )}>
       <Logo />
       <Button variant="outline" asChild>
         <Link href="/login">Login</Link>
