817f5ccada832c9a81b73bfb8a3e010906e19bad
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-08 17:32:17 +0200

    fix(app/dashboard): add the dropdown menu to the account sidebar button

--- src/app/dashboard/components/sidebar/account.tsx
+++ src/app/dashboard/components/sidebar/account.tsx
@@ -1,5 +1,19 @@
-import {UserIcon} from '@/components/ui/icons';
-import {Button} from '@/components/ui';
+import {
+  ChatIcon,
+  HomeIcon,
+  LogoutIcon,
+  SettingsIcon,
+  UserIcon
+} from '@/components/ui/icons';
+import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui';
+import Link from 'next/link';
 
 interface SidebarAccountProps {
   username: string;
@@ -8,10 +22,37 @@ interface SidebarAccountProps {
 export const SidebarAccount = (props: SidebarAccountProps) => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <Button variant="outline" className="m-3 w-full">
-        <UserIcon />
-        <span>{props.username}</span>
-      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="m-3 w-full">
+            <UserIcon />
+            <span>{props.username}</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-48">
+          <DropdownMenuItem>
+            <ChatIcon />
+            <span>Feedback</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/">
+              <HomeIcon />
+              <span>Homepage</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/dashboard/settings">
+              <SettingsIcon />
+              <span>Settings</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="text-red-300 hover:text-red-500">
+            <LogoutIcon />
+            <span>Logout</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
