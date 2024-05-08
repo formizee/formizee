41f5e6b553baf51fa3d1dff5341ed4de4fb851f1
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-09 01:55:14 +0200

    feat(app/legal): create the privacy policy and terms of service pages

---
+++ src/app/legal/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+const PrivacyPolicy = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center overflow-x-clip bg-black">
+      <h1 className="text-3xl font-semibold">Privacy Policy</h1>
+    </main>
+  );
+};
+
+export default PrivacyPolicy;
---
+++ src/app/legal/terms-of-service/page.tsx
@@ -0,0 +1,9 @@
+const TermsOfService = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center overflow-x-clip bg-black">
+      <h1 className="text-3xl font-semibold">Terms of service</h1>
+    </main>
+  );
+};
+
+export default TermsOfService;
