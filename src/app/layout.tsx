660aafdc422cd69c4fde9695d9216f2f26d7e1ff
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-08 19:25:29 +0200

    chore: update public files, favicon and metadata of the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Formizee',
+  title: 'Formizee.',
   description: 'Form Backend built for developers.'
 };
 
