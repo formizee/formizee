7e53cc65cba816c778f061c36c15cfb23b8ea886
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-09 01:54:42 +0200

    fix(app): update login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,43 @@
-import {Logo} from '@/components';
-import {Card} from '@/components/ui';
 import {LoginForm} from '@/components/forms';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Login = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-black p-24">
-      <div className="pointer-events-none absolute inset-0 z-40 flex items-center justify-center bg-white opacity-10 [mask-image:radial-gradient(ellipse_at_center,white_10%,transparent)]"></div>
-      <div className="pointer-events-none absolute inset-0 z-10 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)] dark:bg-black"></div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-black">
+      <div className="pointer-events-none absolute inset-0 z-50 flex items-center justify-center bg-white opacity-5 [mask-image:radial-gradient(ellipse_at_center,white_10%,transparent_60%)]"></div>
+      <div className="pointer-events-none absolute inset-0 z-10 flex justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="bg-grid-pattern pointer-events-none absolute inset-0" />
-      <Logo size="lg" className="z-10 mb-8" />
-      <Card variant="auth" size="auth">
+      <div className="z-20 flex flex-col px-5">
+        <header className="mb-11 flex w-full flex-col items-center gap-10 sm:mb-8 sm:items-start sm:gap-6">
+          <Image
+            className="z-[999] rounded-xl border border-neutral-800 shadow-md shadow-neutral-950"
+            width={64}
+            height={64}
+            src="/logo.svg"
+            alt="Formizee Logo."
+          />
+          <h1 className="bg-gradient-to-b from-white to-neutral-400 bg-clip-text text-xl font-bold text-transparent">
+            Login To Formizee.
+          </h1>
+        </header>
         <LoginForm />
-      </Card>
+        <p className="text-balance text-center text-xs text-neutral-400">
+          {'By signing in, you agree to our '}
+          <Link
+            className="underline underline-offset-2 transition-colors hover:text-neutral-50"
+            href="/legal/terms-of-service">
+            Terms of Service
+          </Link>
+          {' and '}{' '}
+          <Link
+            className="underline underline-offset-2 transition-colors hover:text-neutral-50"
+            href="/legal/privacy-policy">
+            Privacy Policy
+          </Link>
+          .
+        </p>
+      </div>
     </main>
   );
 };
