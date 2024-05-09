1b346228f5c951b72b06b250b1226665f92cd1cd
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-09 13:07:44 +0200

    fix(app/legal): update legal pages

--- src/app/legal/privacy-policy/page.tsx
+++ src/app/legal/privacy-policy/page.tsx
@@ -1,7 +1,14 @@
+import {Navbar, Transition} from '@/components';
+
 const PrivacyPolicy = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center overflow-x-clip bg-black">
-      <h1 className="text-3xl font-semibold">Privacy Policy</h1>
+    <main className="flex min-h-screen flex-col items-center bg-black">
+      <Navbar />
+      <Transition className="mt-20 flex w-[800px] flex-col items-start">
+        <h1 className="bg-gradient-to-b from-white to-neutral-400 bg-clip-text text-4xl font-semibold text-transparent">
+          Privacy Policy
+        </h1>
+      </Transition>
     </main>
   );
 };
--- src/app/legal/terms-of-service/page.tsx
+++ src/app/legal/terms-of-service/page.tsx
@@ -1,7 +1,12 @@
+import { Navbar, Transition } from '@/components';
+
 const TermsOfService = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center overflow-x-clip bg-black">
-      <h1 className="text-3xl font-semibold">Terms of service</h1>
+      <Navbar />
+      <Transition>
+        <h1 className="text-3xl font-semibold">Terms of service</h1>
+      </Transition>
     </main>
   );
 };
