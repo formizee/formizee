58da68b0bde130cdab6e6c5a2e8fa644b18bc1e5
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-09 21:11:08 +0200

    fix(components): update the waitlist form to include the privacy policies on join

--- src/components/forms/waitlist.tsx
+++ src/components/forms/waitlist.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import {zodResolver} from '@hookform/resolvers/zod';
-import {useForm} from 'react-hook-form';
-import {z} from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import Link from 'next/link';
+import { z } from 'zod';
 
-import {MailIcon} from '../ui/icons';
+import { MailIcon } from '../ui/icons';
 import {
   Button,
   Form,
@@ -33,31 +34,46 @@ export const WaitlistForm = () => {
 
   return (
     <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col gap-4 sm:flex-row">
-        <FormField
-          name="email"
-          rules={{required: true}}
-          control={form.control}
-          render={({field}) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  required
-                  type="email"
-                  placeholder="you@mail.com"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button className="gap-x-2">
-          <MailIcon />
-          <span>Join The Waitlist</span>
-        </Button>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col">
+        <div className="flex flex-col gap-4 sm:flex-row">
+          <FormField
+            name="email"
+            rules={{ required: true }}
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    required
+                    type="email"
+                    placeholder="you@mail.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="gap-x-2">
+            <MailIcon />
+            <span>Join The Waitlist</span>
+          </Button>
+        </div>
+        <p className="mt-3 text-balance text-center text-xs text-neutral-400">
+          {'By joining, you agree to our '}
+          <Link
+            className="underline underline-offset-2 transition-colors hover:text-neutral-50"
+            href="/legal/terms-of-service">
+            Terms of Service
+          </Link>
+          {' and '}{' '}
+          <Link
+            className="underline underline-offset-2 transition-colors hover:text-neutral-50"
+            href="/legal/privacy-policy">
+            Privacy Policy
+          </Link>
+          .
+        </p>
       </form>
     </Form>
   );
