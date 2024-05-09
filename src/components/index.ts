3a556e717a9c028aa35dafafcbe15ea5ac5b2999
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-09 21:10:37 +0200

    fix(components): forget to export card in the barrel index

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export {Transition} from './transition';
 export {Spotlight} from './spotlight';
 export {BaseBody} from './base-body';
 export {Navbar} from './navbar';
+export {Cards} from './cards';
 export {Logo} from './logo';
