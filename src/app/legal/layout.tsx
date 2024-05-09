7ba4cd2fea9fe2161914ef7134fa522b23e4bc79
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-09 15:18:36 +0200

    fix(app/legal): update terms of service page

---
+++ src/app/legal/layout.tsx
@@ -0,0 +1,16 @@
+import {Navbar, Transition} from '@/components';
+
+const LegalLayout = ({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center overflow-x-clip bg-black">
+      <Navbar />
+      <Transition className="mx-6 my-28 max-w-[800px]">{children}</Transition>
+    </main>
+  );
+};
+
+export default LegalLayout;
