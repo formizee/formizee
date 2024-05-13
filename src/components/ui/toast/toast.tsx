7a346c5ddcc601c149493e52ebd9144658203f78
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-13 20:49:53 +0200

    fix(components/ui): update toast destructive style

--- src/components/ui/toast/toast.tsx
+++ src/components/ui/toast/toast.tsx
@@ -32,7 +32,7 @@ const toastVariants = cva(
         success:
           'destructive group border-green-500 bg-green-500 text-neutral-50 dark:border-green-900 dark:bg-green-400/10 dark:text-neutral-50',
         destructive:
-          'destructive group border-red-500 bg-red-500 text-neutral-50 dark:border-red-900 dark:bg-red-900 dark:text-neutral-50'
+          'destructive group border-red-500 bg-red-500 text-neutral-50 dark:border-red-900 dark:bg-red-400/10 dark:text-neutral-50'
       }
     },
     defaultVariants: {
