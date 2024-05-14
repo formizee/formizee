cd5a393efae5c774d6f598112f259a92278fcc10
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-14 18:10:55 +0200

    fix(domain): update auth service to resolve a AuthResponse object instead of directly the user, this is better for handling errors

--- src/domain/services/auth.ts
+++ src/domain/services/auth.ts
@@ -1,15 +1,25 @@
-import {Name, Email, Password} from '../models/values';
-import {User} from '../models';
+import {Name, Email, Password} from '@/domain/models/values';
+import {User} from '@/domain/models/user';
+
+export type AuthResponse =
+  | {
+      data: {user: User};
+      error: null;
+    }
+  | {
+      data: null;
+      error: Error;
+    };
 
 export type AuthServiceLogin = (
   email: Email,
   password: string
-) => Promise<User>;
+) => Promise<AuthResponse>;
 
 export type AuthServiceRegister = (
   name: Name,
   email: Email,
   password: Password
-) => Promise<User>;
+) => Promise<AuthResponse>;
 
 export type AuthServiceLogout = () => Promise<void>;
