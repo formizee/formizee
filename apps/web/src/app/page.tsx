26e9079d78de93889a9a12a1776cfc428ed3c5f6
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-14 23:15:52 +0200

    fix(app): update main page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,12 +3,12 @@ import {WaitlistForm} from '@/components/forms';
 
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center overflow-x-clip bg-black lg:justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-between overflow-x-clip bg-black lg:h-screen lg:justify-center">
       <Spotlight className="absolute -left-10 -top-40 lg:-left-0" />
       <div className="pointer-events-none absolute inset-0 z-10 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
       <div className="bg-grid-pattern pointer-events-none absolute inset-0" />
       <Navbar />
-      <header className="z-10 flex h-[calc(100dvh-64px)] w-full flex-col-reverse justify-end lg:flex-row lg:items-center">
+      <header className="z-10 flex w-full flex-col-reverse justify-evenly lg:h-full lg:flex-row lg:items-center lg:justify-between">
         <div className="ml-4 flex flex-col items-start gap-y-4 sm:ml-8 md:gap-y-8 lg:ml-8 xl:ml-24 2xl:ml-64">
           <h1 className="bg-gradient-to-b from-white to-slate-400 bg-clip-text text-3xl font-semibold leading-[1.2] text-transparent sm:text-5xl">
             Forms Backend
