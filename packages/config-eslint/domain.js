d0a6640c21bce8c74b5cb4ce8f38d951b8482d7c
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-15 13:28:50 +0200

    fix(packages/config-eslint): update eslint rules

--- packages/config-eslint/domain.js
+++ packages/config-eslint/domain.js
@@ -3,8 +3,8 @@ const { resolve } = require('node:path');
 const project = resolve(process.cwd(), 'tsconfig.json');
 
 /*
- * This is a custom ESLint configuration for use a library
- * that utilizes React.
+ * This is a custom ESLint configuration for use with
+ * typescript packages.
  *
  * This config extends the Vercel Engineering Style Guide.
  * For more information, see https://github.com/vercel/style-guide
@@ -13,15 +13,12 @@ const project = resolve(process.cwd(), 'tsconfig.json');
 
 module.exports = {
   extends: [
-    '@vercel/style-guide/eslint/browser',
     '@vercel/style-guide/eslint/typescript',
-    '@vercel/style-guide/eslint/react'
+    '@vercel/style-guide/eslint/node',
+    'eslint-config-turbo'
   ].map(require.resolve),
   parserOptions: {
     project
-  },
-  globals: {
-    JSX: true
   },
   settings: {
     'import/resolver': {
@@ -33,17 +30,5 @@ module.exports = {
       }
     }
   },
-  ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.js', '**/*.css'],
-  // add rules configurations here
-  rules: {
-    'import/no-default-export': 'off'
-  },
-  overrides: [
-    {
-      files: ['*.config.js'],
-      env: {
-        node: true
-      }
-    }
-  ]
+  ignorePatterns: ['node_modules/', 'dist/']
 };
