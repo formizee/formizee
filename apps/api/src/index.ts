ec1cca32a47b9c3034b8443e1dbead90e2dc8221
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-21 20:19:43 +0200

    feat(apps/api): create a session auth system (needs a more work)

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,19 @@
+import { DatabaseConfigProvider } from '@/lib/database';
 import { Hono } from 'hono'
 
-import {usersRouter} from '@/routes/users';
-import { DatabaseConfigProvider } from '@/lib/database';
+import apiRouter from '@/routes/api';
+import authRouter from '@/routes/auth';
 
 export type Env = {
   DB: D1Database;
+  SESSION_SECRET: string;
 }
 
 const router = new Hono<{Bindings: Env}>();
 
-router.route('/users', usersRouter);
+
+router.route('/api', apiRouter);
+router.route('/auth', authRouter);
 
 export default {
   fetch(request: Request, env: Env, ctx: ExecutionContext) {
