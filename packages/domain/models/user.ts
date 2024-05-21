5a36b468e36ddb393c35c75f88671b7134bfce74
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-21 13:09:02 +0200

    fix(packages/domain): update user model to accept more parameters as constructor

--- packages/domain/models/user.ts
+++ packages/domain/models/user.ts
@@ -4,13 +4,25 @@ export class User {
   private readonly _uid: Uid;
   private readonly _name: Name;
   private readonly _email: Email;
-  private readonly _linkedEmails: Email[];
+  private readonly _forms: Uid[] = [];
+  private readonly _linkedEmails: Email[] = [];
 
-  constructor(uid: string, name: string, email: string) {
+  constructor(uid: string, name: string, email: string, forms?: string[], linkedEmails?: string[]) {
     this._uid = new Uid(uid);
     this._name = new Name(name);
     this._email = new Email(email);
-    this._linkedEmails = [this._email];
+
+    if(forms && forms.length > 0) {
+      forms.forEach(form => {
+        this._forms.push(new Uid(form));
+      })
+    }
+
+    if(linkedEmails) {
+      linkedEmails.forEach(email => {
+        this._linkedEmails.push(new Email(email));
+      })
+    } else this._linkedEmails = [this._email];
   }
 
   get uid(): string {
