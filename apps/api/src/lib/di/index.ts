db14fb79faf7be80e1b0aba1490e8fd9734d3289
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-22 01:02:05 +0200

    fix(apps/api): forget to add the auth service to the di container

--- apps/api/src/lib/di/index.ts
+++ apps/api/src/lib/di/index.ts
@@ -6,13 +6,19 @@ import {
   EndpointsRepositoryImplementation,
   UsersRepositoryImplementation
 } from '@/data/repositories';
+import { AuthServiceImplementation } from '@/data/services';
 
 const container = createContainer<Dependencies>();
 
 container.register({
+
+  /* Repositories */
   submissionsRepository: asClass(SubmissionsRepositoryImplementation),
   endpointsRepository: asClass(EndpointsRepositoryImplementation),
-  usersRepository: asClass(UsersRepositoryImplementation)
+  usersRepository: asClass(UsersRepositoryImplementation),
+
+  /* Services */
+  authService: asClass(AuthServiceImplementation)
 });
 
 export const resolve = container.resolve;
