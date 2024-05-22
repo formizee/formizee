1775f5e264eab89a27d9644fcc6550a4cff86094
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-22 21:14:37 +0200

    feat(apps/api): add the resetPassword method to the authService

--- packages/domain/services/auth/index.ts
+++ packages/domain/services/auth/index.ts
@@ -5,8 +5,10 @@ import type {Response, User} from '@/models';
 export interface AuthService {
   login(email: Email, password: string): Promise<Response<User>>;
   register(name: Name, email: Email, password: Password): Promise<Response<User>>;
-  verifyUser(email: Email, token: string): Promise<Response<User>>;
+
   sendVerification(email: Email): Promise<Response<true>>;
+  verifyUser(email: Email, token: string): Promise<Response<User>>;
+  resetPassword(email: Email, token: string): Promise<Response<User>>;
 }
 
 export type AuthServiceLogin = (
