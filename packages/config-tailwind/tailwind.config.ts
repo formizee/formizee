35271f0a6a6f08742f2633a58e45749e0d2ce9f8
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-24 14:45:21 +0200

    feat(packages/config-tailwind): add a new animation for the input-otp component

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -30,6 +30,10 @@ const config: Omit<Config, 'content'> = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' }
         },
+        'caret-blink': {
+          '0%,70%,100%': { opacity: '1' },
+          '20%,50%': { opacity: '0' },
+        },
         'fade-in': {
           '0%': { opacity: '0', transform: 'translateY(10px)' },
           '100%': { opacity: '1', transform: 'translateY(0px)' }
@@ -48,6 +52,7 @@ const config: Omit<Config, 'content'> = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'caret-blink': 'caret-blink 1.25s ease-out infinite',
         'fade-in': 'fade-in 400ms ease-out forwards',
         spotlight: 'spotlight 1.5s ease-out forwards'
       }
