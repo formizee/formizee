a6c7fd8174a5cc3f5ded494e7574e2a347529b1a
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-24 14:12:47 +0200

    fix(packages/domain): remove the weird domain services with functional actions

--- packages/domain/services/waitlist.ts
+++ packages/domain/services/waitlist.ts
@@ -4,13 +4,3 @@ import {Response} from '@/models';
 export interface WaitlistService {
   join(email: Email): Promise<Response<true>>;
 }
-
-export type WaitlistResponse =
-  | {
-      error: null;
-    }
-  | {
-      error: Error;
-    };
-
-export type WaitlistServiceJoin = (email: Email) => Promise<WaitlistResponse>;
