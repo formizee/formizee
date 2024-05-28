49d79c242e69957cda79f34b31e89c6559d84455
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-28 16:42:42 +0200

    fix(packages/ui): update toaster pointer event to prevent overlapping

--- packages/ui/src/components/toast/toast.tsx
+++ packages/ui/src/components/toast/toast.tsx
@@ -12,7 +12,7 @@ const ToastViewport = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Viewport
     className={cn(
-      'fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:left-0 sm:top-auto sm:flex-col md:max-w-[460px]',
+      'fixed top-0 z-[100] pointer-events-none flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:left-0 sm:top-auto sm:flex-col md:max-w-[460px]',
       className
     )}
     ref={ref}
