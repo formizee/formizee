58f897ed50decea51427cd6c11d6020ea7e97463
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-30 14:52:11 +0200

    fix(apps/web): update the dashboard sidebar props

--- apps/web/src/app/dashboard/components/sidebar/index.tsx
+++ apps/web/src/app/dashboard/components/sidebar/index.tsx
@@ -2,50 +2,11 @@ import CreateFormButton from './create';
 import Account from './account';
 import Forms from './forms';
 
-const FORMS = [
-  {
-    uid: '1',
-    name: 'My homepage'
-  },
-  {
-    uid: '2',
-    name: 'Another Project'
-  },
-  {
-    uid: '13',
-    name: 'My Bussiness Saas'
-  },
-  {
-    uid: '14',
-    name: 'My homepage askljfaklsdjflkasjdklfj'
-  },
-  {
-    uid: '15',
-    name: 'Another Project'
-  },
-  {
-    uid: '16',
-    name: 'My Bussiness Saas'
-  },
-  {
-    uid: '17',
-    name: 'My homepage'
-  },
-  {
-    uid: '18',
-    name: 'Another Project'
-  },
-  {
-    uid: '19',
-    name: 'My Bussiness Saas'
-  }
-];
-
 export function DashboardSidebar(): JSX.Element {
   return (
     <div className="flex w-60 flex-col justify-between border-r border-r-neutral-800 px-2">
       <CreateFormButton />
-      <Forms forms={FORMS} />
+      <Forms forms={[]} />
       <Account username="@pauchiner" />
     </div>
   );
