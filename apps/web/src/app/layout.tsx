09398749f7f3f574d6eccdfecf8991d972103259
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-05-31 20:30:40 +0200

    fix(apps/web): update webmanifest and icons

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
     siteName: 'The open source Formspree alternative',
     url: 'https://formizee.com'
   },
+  themeColor: '#0a0a0a',
   metadataBase: new URL('https://formizee.com')
 };
 
--- apps/web/src/app/manifest.ts
+++ apps/web/src/app/manifest.ts
@@ -8,12 +8,27 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/',
     display: 'standalone',
     background_color: '#0a0a0a',
-    theme_color: '#fbbf24',
+    theme_color: '#0a0a0a',
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon'
+        src: '/icons/icon-192x192.png',
+        sizes: "192x192",
+        type: 'image/png'
+      },
+      {
+        src: '/icons/icon-256x256.png',
+        sizes: "256x256",
+        type: 'image/png'
+      },
+      {
+        src: '/icons/icon-384x384.png',
+        sizes: "384x384",
+        type: 'image/png'
+      },
+      {
+        src: '/icons/icon-512x512.png',
+        sizes: "512x512",
+        type: 'image/png'
       }
     ]
   };
