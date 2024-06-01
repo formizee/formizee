a788861e04c5e7c05c5b5d33f308d794e229f56d
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-01 16:39:51 +0200

    fix(apps/web): update cards breakpoints to not wrap the homepage heading

--- apps/web/src/app/(home)/components/cards/index.tsx
+++ apps/web/src/app/(home)/components/cards/index.tsx
@@ -4,7 +4,7 @@ import {CodeCard} from './code';
 
 export function Cards(): JSX.Element {
   return (
-    <div className="relative mt-20 h-72 w-full transform-gpu overflow-x-clip sm:h-80 md:h-96 lg:w-1/2 xl:w-[55%] 2xl:w-[55%]">
+    <div className="relative mt-20 h-72 bg-blue-400 w-full transform-gpu sm:h-80 md:h-96 lg:w-[48%] xl:w-[53%] 2xl:w-[55%]">
       <div className="absolute mt-5 h-[256px] scale-90 sm:mt-10 sm:scale-100 md:mt-16 lg:mt-10">
         <CodeCard />
         <ProcessCard />
