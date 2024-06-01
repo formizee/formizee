3d131ed0c07b53414f358d13ae5776a3520cf9de
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-01 13:15:38 +0200

    fix(apps/web): finally complete the requirements to use web as wpa

--- apps/web/src/app/manifest.ts
+++ apps/web/src/app/manifest.ts
@@ -11,22 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000',
     icons: [
       {
-        src: '/icons/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png'
-      },
-      {
-        src: '/icons/icon-256x256.png',
-        sizes: '256x256',
-        type: 'image/png'
-      },
-      {
-        src: '/icons/icon-384x384.png',
-        sizes: '384x384',
-        type: 'image/png'
-      },
-      {
-        src: '/icons/icon-512x512.png',
+        src: '/icon.png',
         sizes: '512x512',
         type: 'image/png'
       }
