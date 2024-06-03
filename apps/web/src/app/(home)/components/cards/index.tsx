2a1dc076b2acb8966b1fa047276f314e1b3ce62f
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-04 01:09:12 +0200

    fix(apps/web): resolve a overflow issue with the new card variants

--- apps/web/src/app/(home)/components/cards/index.tsx
+++ apps/web/src/app/(home)/components/cards/index.tsx
@@ -4,7 +4,7 @@ import {ProcessCard} from './process';
 
 export function Cards(): JSX.Element {
   return (
-    <div className="relative mt-20 h-72 w-full transform-gpu sm:h-80 md:h-96 lg:w-[48%] xl:w-[53%] 2xl:w-[55%]">
+    <div className="relative overflow-hidden mt-20 h-72 w-full transform-gpu sm:h-80 md:h-96 lg:w-[48%] xl:w-[53%] 2xl:w-[55%]">
       <div className="absolute mt-0 h-[256px] scale-90 sm:mt-10 sm:scale-100 md:mt-16 lg:mt-10">
         <CodeCard />
         <ProcessCard />
