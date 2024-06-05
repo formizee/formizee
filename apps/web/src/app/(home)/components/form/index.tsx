37e0963c7d3b708eb2d8e852aae37fb3fe2db871
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-05 11:42:43 +0200

    style: format with prettier tailwindcss

--- apps/web/src/app/(home)/components/form/index.tsx
+++ apps/web/src/app/(home)/components/form/index.tsx
@@ -68,13 +68,13 @@ function FormFields(): JSX.Element {
       <p className="mb-3 mt-3 text-start text-xs text-neutral-400">
         {'By joining, you agree to our '}
         <Link
-          className="underline underline-offset-2 transition-colors hover:text-neutral-50 whitespace-nowrap"
+          className="whitespace-nowrap underline underline-offset-2 transition-colors hover:text-neutral-50"
           href="/legal/terms-of-service">
           Terms of Service
         </Link>
         {' and '}{' '}
         <Link
-          className="underline underline-offset-2 transition-colors hover:text-neutral-50 whitespace-nowrap"
+          className="whitespace-nowrap underline underline-offset-2 transition-colors hover:text-neutral-50"
           href="/legal/privacy-policy">
           Privacy Policy
         </Link>
