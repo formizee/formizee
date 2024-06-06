8e15ce757ef1f76b0b35027536ddc5db7e81cce0
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-06 19:47:44 +0200

    chore(apps/node-api): configure drizzle to work with vercel postgres

--- apps/node-api/drizzle.config.ts
+++ apps/node-api/drizzle.config.ts
@@ -1,12 +1,11 @@
 import {defineConfig} from 'drizzle-kit';
+import '@/lib/enviroment';
 
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.POSTGRES_URL!,
-    host: process.env.POSTGRES_HOST!,
-    database: process.env.POSTGRES_DATABASE!
+    url: process.env.POSTGRES_URL!
   }
 });
