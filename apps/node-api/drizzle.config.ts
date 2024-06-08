5f4a5438c18d2c54680e23890a0c5e3a20888214
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-08 11:49:45 +0200

    fix(apps/node-api): update drizzle to use pg client
    Instead of using @vercel/postgres start using pg
    to switch quickly between different providers.

--- apps/node-api/drizzle.config.ts
+++ apps/node-api/drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   out: './drizzle/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.POSTGRES_URL!
+    url: process.env.DATABASE_URL!
   }
 });
---
+++ apps/node-api/drizzle/index.ts
@@ -0,0 +1,21 @@
+import {drizzle} from 'drizzle-orm/node-postgres';
+import {eq as compare} from 'drizzle-orm';
+import {Client} from 'pg';
+import * as schemas from './schema';
+import '@/lib/enviroment';
+
+// Schemas
+export const users = schemas.users;
+export const waitlist = schemas.waitlist;
+export const endpoints = schemas.endpoints;
+export const authTokens = schemas.authTokens;
+export const submissions = schemas.submissions;
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL!
+});
+await client.connect();
+
+// Drizzle Utilities
+export const db = drizzle(client, {schema: schemas});
+export const eq = compare;
