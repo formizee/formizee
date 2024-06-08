1da429b4c9c03ed19fe01abf1ce679da828a9bdd
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-08 22:52:32 +0200

    fix(apps/node-api): remove the https createServer

--- apps/node-api/src/lib/server/index.ts
+++ apps/node-api/src/lib/server/index.ts
@@ -1,44 +1,36 @@
 import {performance} from 'perf_hooks';
-import {createServer} from 'node:https';
 import {serve} from '@hono/node-server';
 import type {Hono} from 'hono';
 import {loadedEnviroments} from '@/lib/enviroment';
 import {version} from '@/../package.json';
 
 export const server = (app: Hono): void => {
+  const startTime = performance.now();
+
   const BASE_URL = process.env.BASE_URL ?? 'http://localhost';
   const PORT = Number(process.env.PORT ?? 3000);
 
-  const run = (port?: number): void => {
-    const startTime = performance.now();
+  let enviroments = '';
+  loadedEnviroments.forEach((file, index) => {
+    enviroments += file;
+    if (index !== loadedEnviroments.length - 1) {
+      enviroments += ', ';
+    }
+  });
 
-    const currentPort = port ?? PORT;
+  console.info(`\x1b[1m ▲ Formizee API ${version}\x1b[0m`);
+  console.info(
+    ` - Local:        \x1b[33m\x1b[4m${BASE_URL}:${PORT.toString()}\x1b[0m`
+  );
+  console.info(` - Enviroments:  ${enviroments}\n`);
+  console.info(`\x1b[32m ✓ \x1b[0m Starting...`);
 
-    let enviroments = '';
-    loadedEnviroments.forEach((file, index) => {
-      enviroments += file;
-      if (index !== loadedEnviroments.length - 1) {
-        enviroments += ', ';
-      }
-    });
+  serve({
+    fetch: app.fetch,
+    port: PORT
+  });
 
-    console.info(`\x1b[1m ▲ Formizee API ${version}\x1b[0m`);
-    console.info(
-      ` - Local:        \x1b[33m\x1b[4m${BASE_URL}:${currentPort.toString()}\x1b[0m`
-    );
-    console.info(` - Enviroments:  ${enviroments}\n`);
-    console.info(`\x1b[32m ✓ \x1b[0m Starting...`);
-
-    serve({
-      fetch: app.fetch,
-      createServer,
-      port: currentPort
-    });
-
-    const endTime = performance.now();
-    const time = Math.round(endTime - startTime).toString();
-    console.info(`\x1b[32m ✓ \x1b[0m Ready in ${time}ms`);
-  };
-
-  run();
+  const endTime = performance.now();
+  const time = Math.round(endTime - startTime).toString();
+  console.info(`\x1b[32m ✓ \x1b[0m Ready in ${time}ms\n`);
 };
