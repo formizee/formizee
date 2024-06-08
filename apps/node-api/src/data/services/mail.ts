739aeee52eba1c68c1e42507b28a6de5f0532ae3
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-08 14:45:19 +0200

    feat(apps/node-api): create the mail service and its use case

---
+++ apps/node-api/src/data/services/mail.ts
@@ -0,0 +1,29 @@
+import {type Transporter, createTransport} from 'nodemailer';
+import type {MailService as Service} from 'domain/services';
+import {type Mail, Response} from 'domain/models';
+import type {Uid} from 'domain/models/values';
+
+export class MailService implements Service {
+  private readonly smtp: Transporter;
+
+  constructor() {
+    this.smtp = createTransport({
+      host: process.env.SMTP_HOST!,
+      auth: {
+        user: process.env.SMTP_USER!,
+        pass: process.env.SMTP_PASSWORD!
+      }
+    });
+  }
+
+  async send(mail: Mail): Promise<Response<Uid>> {
+    await this.smtp.sendMail({
+      from: `${mail.name} <${mail.from}>`,
+      to: mail.to,
+      subject: mail.subject,
+      html: mail.html
+    });
+
+    return Response.error('miau');
+  }
+}
---
+++ apps/node-api/src/useCases/mail/index.ts
@@ -0,0 +1,1 @@
+export {MailSend} from './send';
