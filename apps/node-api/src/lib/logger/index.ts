815fde86df6795558ba83ffc0bf56fd8f5339955
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-08 22:51:42 +0200

    feat(apps/node-api): add logger lib

---
+++ apps/node-api/src/lib/logger/index.ts
@@ -0,0 +1,27 @@
+import {logger as honoLogger} from 'hono/logger';
+
+const messages = (message: string): void => {
+  const rawData = message.split(' ');
+  const requestType = rawData[3];
+
+  const generateContent = (data: string[]): string => {
+    data.splice(0, 4);
+    return data.join(' ');
+  };
+
+  const generateType = (type: string): string => {
+    if (type === 'GET') return '\x1b[32mGET\x1b[0m';
+    if (type === 'POST') return '\x1b[34mPOST\x1b[0m';
+    if (type === 'PUT') return '\x1b[33mPUT\x1b[0m';
+    if (type === 'PATCH') return '\x1b[35mPATCH\x1b[0m';
+    if (type === 'DELETE') return '\x1b[31mDELETE\x1b[0m';
+    return type;
+  };
+
+  const content = generateContent(rawData);
+  if (message.includes('->'))
+    console.log(`${generateType(requestType)} -> ${content}\n`);
+  else console.log(`${generateType(requestType)} <- ${content}`);
+};
+
+export const logger = honoLogger(messages);
