7f397b09dea42f568f9b4747a05fde3d32747626
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-08 16:37:52 +0200

    fix(apps/node-api): add new functions for the enviroment lib
    add a function to trace where files are used by node as enviroment

--- apps/node-api/src/lib/enviroment/index.ts
+++ apps/node-api/src/lib/enviroment/index.ts
@@ -1,5 +1,23 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import * as dotenv from 'dotenv';
 
+const envFiles = [
+  '.env.local',
+  '.env.development.local',
+  '.env.development',
+  '.env'
+];
+
+export const loadedEnviroments: string[] = [];
+
+envFiles.forEach(file => {
+  const envPath = path.resolve(process.cwd(), file);
+  if (fs.existsSync(envPath)) {
+    loadedEnviroments.push(file);
+  }
+});
+
 dotenv.config({
-  path: ['.env.local', '.env.production', '.env']
+  path: envFiles
 });
