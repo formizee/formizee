855c8c16660ec4afa84b7fd82e76d1f82eabf3ac
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-10 23:16:30 +0200

    fix(packages/domain): update endpoint repository save use case

--- packages/domain/repositories/endpoints.ts
+++ packages/domain/repositories/endpoints.ts
@@ -4,11 +4,7 @@ import type {Uid, Email} from '../models/values';
 export interface EndpointsRepository {
   load: (uid: Uid) => Promise<Response<Endpoint>>;
   loadAll: (owner: Uid) => Promise<Response<Endpoint[]>>;
-  save: (
-    name: string,
-    owner: Uid,
-    targetEmail: Email
-  ) => Promise<Response<Endpoint>>;
+  save: (name: string, owner: Uid) => Promise<Response<Endpoint>>;
   delete: (uid: Uid) => Promise<Response<true>>;
 
   updateName: (uid: Uid, name: string) => Promise<Response<Endpoint>>;
