6ee8a8204180106a639408bf18e5f3f399872871
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-10 18:20:37 +0200

    feat(apps/node-api): set timeout middleware for routes

--- apps/node-api/src/index.ts
+++ apps/node-api/src/index.ts
@@ -1,10 +1,12 @@
 import {Hono} from 'hono';
+import {timeout} from 'hono/timeout';
 import {logger} from '@/lib/logger';
 import {server} from '@/lib/server';
 import {waitlist} from '@/routes/waitlist';
 import {endpoints} from './routes/endpoints';
 
 const router = new Hono().basePath('/v1');
+router.use(timeout(30 * 1000));
 router.use(logger);
 
 router.route('/waitlist', waitlist);
