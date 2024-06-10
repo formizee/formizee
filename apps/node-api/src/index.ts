326797074dc7651e02876e90bab3bc233f0cde58
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-10 12:46:13 +0200

    feat(apps/node-api): use logger

--- apps/node-api/src/index.ts
+++ apps/node-api/src/index.ts
@@ -1,7 +1,9 @@
 import {Hono} from 'hono';
+import {logger} from '@/lib/logger';
 import {server} from '@/lib/server';
 
 const router = new Hono().basePath('/v1');
+router.use(logger);
 
 router.get('/status', context => context.text('OK', 200));
 
---
+++ apps/node-api/src/routes/waitlist/index.ts
@@ -0,0 +1,22 @@
+import type {StatusCode} from 'hono/utils/http-status';
+import {zValidator} from '@hono/zod-validator';
+import {Hono} from 'hono';
+import {JoinWaitlist} from '@/useCases/waitlist';
+import {joinSchema} from './schema';
+
+export const waitlist = new Hono();
+
+waitlist.post('/join', zValidator('json', joinSchema), async context => {
+  const {email} = context.req.valid('json');
+
+  const service = new JoinWaitlist(email);
+  const response = await service.run();
+
+  if (!response.ok) {
+    return context.json(response.error, response.status as StatusCode);
+  }
+
+  return context.json(response.body, response.status as StatusCode);
+});
+
+export default waitlist;
