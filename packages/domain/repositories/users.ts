6530ee0ed6c3e891111140ec9c7176e53ae049fd
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-11 10:28:30 +0200

    fix(packages/domain): update users repository delete use case
    Now delete action requires the user password to be acomplished.

--- packages/domain/repositories/users.ts
+++ packages/domain/repositories/users.ts
@@ -3,7 +3,7 @@ import type {User, Response} from '../models';
 
 export interface UsersRepository {
   load: (identifier: Uid | Email) => Promise<Response<User>>;
-  delete: (uid: Uid) => Promise<Response<true>>;
+  delete: (uid: Uid, password: string) => Promise<Response<true>>;
   updateName: (uid: Uid, newName: Name) => Promise<Response<User>>;
   updateEmail: (uid: Uid, newEmail: Email) => Promise<Response<User>>;
   updatePassword: (uid: Uid, newPassword: Password) => Promise<Response<User>>;
