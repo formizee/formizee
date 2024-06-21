beee894f7b0c384068434ec2969def76f40d7686
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-21 19:13:38 +0200

    fix(apps/api): update lib utils to match the new schemas

--- apps/api/src/lib/utils/index.ts
+++ apps/api/src/lib/utils/index.ts
@@ -1,34 +1,28 @@
-import {Endpoint, Submission, User} from 'domain/models';
+import {Endpoint, Submission, Team, TeamPlan, User} from 'domain/models';
 
-export const createUser = (data: {
+export const createTeam = (data: {
   id: string;
   name: string;
-  email: string;
-  isVerified: boolean;
-  forms: string[];
-  linkedEmails: {email: string; isVerified: boolean}[];
-  permission: 'user' | 'admin';
+  plan: TeamPlan;
+  availableEmails: string[];
   createdAt: Date;
   updatedAt: Date;
-}): User => {
-  return new User(
+}): Team => {
+  return new Team(
     data.id,
     data.name,
-    data.email,
-    data.isVerified,
-    data.permission,
+    data.plan,
+    data.availableEmails,
     data.createdAt,
-    data.updatedAt,
-    data.forms,
-    data.linkedEmails
+    data.updatedAt
   );
 };
 
 export const createEndpoint = (data: {
   id: string;
   name: string;
-  owner: string;
-  targetEmail: string;
+  team: string;
+  targetEmails: string[];
   redirectUrl: string;
   isEnabled: boolean;
   emailNotifications: boolean;
@@ -38,8 +32,8 @@ export const createEndpoint = (data: {
   return new Endpoint(
     data.id,
     data.name,
-    data.owner,
-    data.targetEmail,
+    data.team,
+    data.targetEmails,
     data.redirectUrl,
     data.isEnabled,
     data.emailNotifications,
@@ -49,17 +43,45 @@ export const createEndpoint = (data: {
 };
 
 export const createSubmission = (data: {
-  id: number;
+  id: string;
   endpoint: string;
   data: object;
-  createdAt: Date;
   isSpam: boolean;
+  isRead: boolean;
+  createdAt: Date;
 }): Submission => {
   return new Submission(
-    data.id.toString(),
+    data.id,
     data.endpoint,
     data.data,
+    data.isSpam,
+    data.isRead,
+    data.createdAt
+  );
+};
+
+export const createUser = (
+  data: {
+    id: string;
+    name: string;
+    email: string;
+    isVerified: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  },
+  linkedEmails: {email: string; isVerified: boolean}[]
+): User => {
+  const emails = linkedEmails.map(item => {
+    return {email: item.email, isVerified: item.isVerified};
+  });
+
+  return new User(
+    data.id,
+    data.name,
+    data.email,
+    data.isVerified,
+    emails,
     data.createdAt,
-    data.isSpam
+    data.updatedAt
   );
 };
