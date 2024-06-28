4d60de95161409f309de450eb57e380b0e0b4c8d
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-06-28 16:51:28 +0200

    feat(packages/domain): create member model

--- packages/domain/models/index.ts
+++ packages/domain/models/index.ts
@@ -1,6 +1,7 @@
 export {Submission} from './submission';
 export {Response} from './response';
 export {Endpoint} from './endpoint';
+export {Member} from './member';
 export {User} from './user';
 export {Team} from './team';
 export {Mail} from './mail';
