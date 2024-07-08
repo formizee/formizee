67497c5beb8e792fd767ca8b8452994fdeef7f67
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-08 22:08:50 +0200

    test(apps/api): add the waitlist join integration test

---
+++ apps/api/tests/integration/waitlist/join.test.ts
@@ -0,0 +1,39 @@
+import {it, expect, describe} from 'vitest';
+import {waitlist} from '@/routes/waitlist';
+
+describe('Waitlist /join integration', () => {
+  it('Should return 200', async () => {
+    const response = await waitlist.request('/join', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: 'example@formizee.com'
+      })
+    });
+
+    expect(await response.json()).toBe("You're on the Waitlist!");
+    expect(response.status).toBe(201);
+  });
+
+  it('Should return 409 when the user is already on the waitlist', async () => {
+    const response = await waitlist.request('/join', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: 'example@formizee.com'
+      })
+    });
+
+    const expectedResponse = {
+      name: 'Be patient...',
+      description:
+        "You're already on the list! We'll keep you updated on Formizee launch."
+    };
+    expect(await response.json()).toStrictEqual(expectedResponse);
+    expect(response.status).toBe(409);
+  });
+});
