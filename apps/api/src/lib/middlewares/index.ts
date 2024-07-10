30c99c3f94c19f0269587a632e34b51c971b7505
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-10 22:48:34 +0200

    style(apps/api): format with prettier

--- apps/api/src/lib/middlewares/index.ts
+++ apps/api/src/lib/middlewares/index.ts
@@ -1,17 +1,17 @@
-import type { OpenAPIHono } from "@hono/zod-openapi";
-import { secureHeaders } from "hono/secure-headers";
-import { rateLimiter } from "hono-rate-limiter";
-import { csrf } from "hono/csrf";
-import { bodyLimit } from "./body-limit";
-import { timeout } from "./timeout";
-import { logger } from "./logger";
+import type {OpenAPIHono} from '@hono/zod-openapi';
+import {secureHeaders} from 'hono/secure-headers';
+import {rateLimiter} from 'hono-rate-limiter';
+import {csrf} from 'hono/csrf';
+import {bodyLimit} from './body-limit';
+import {timeout} from './timeout';
+import {logger} from './logger';
 
 export const limiter = rateLimiter({
   windowMs: 60 * 1000,
   limit: 60,
-  standardHeaders: "draft-6",
-  keyGenerator: (c) => c.req.header("x-forwarded-for") ?? ""
-})
+  standardHeaders: 'draft-6',
+  keyGenerator: c => c.req.header('x-forwarded-for') ?? ''
+});
 
 export const security = (router: OpenAPIHono): void => {
   router.use(secureHeaders());
@@ -20,4 +20,4 @@ export const security = (router: OpenAPIHono): void => {
   router.use(timeout);
   router.use(logger);
   router.use(csrf());
-}
+};
