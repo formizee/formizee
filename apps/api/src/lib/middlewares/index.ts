2a4a697c12223fea04822357ec160bc49aabd628
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-11 01:27:16 +0200

    feat(apps/api): implement cors middleware

--- apps/api/src/lib/middlewares/index.ts
+++ apps/api/src/lib/middlewares/index.ts
@@ -1,12 +1,13 @@
 import type {OpenAPIHono} from '@hono/zod-openapi';
 import {secureHeaders} from 'hono/secure-headers';
 import {rateLimiter} from 'hono-rate-limiter';
+import {cors} from 'hono/cors';
 import {csrf} from 'hono/csrf';
 import {bodyLimit} from './body-limit';
 import {timeout} from './timeout';
 import {logger} from './logger';
 
-export const limiter = rateLimiter({
+const limiter = rateLimiter({
   windowMs: 60 * 1000,
   limit: 60,
   standardHeaders: 'draft-6',
@@ -14,10 +15,17 @@ export const limiter = rateLimiter({
 });
 
 export const security = (router: OpenAPIHono): void => {
+  if(!process.env.WEB_URL) throw new Error("WEB_URL enviroment variable is not defined.")
+
   router.use(secureHeaders());
   router.use(bodyLimit);
   router.use(limiter);
   router.use(timeout);
   router.use(logger);
   router.use(csrf());
+  router.use(cors({
+    origin: process.env.WEB_URL,
+    allowMethods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: true
+  }))
 };
