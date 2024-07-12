e705056ece8c76920b670d9c3df2bbe1628e12d4
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-12 13:26:48 +0200

    style(apps/api): format with prettier

--- apps/api/src/lib/middlewares/rate-limiter.ts
+++ apps/api/src/lib/middlewares/rate-limiter.ts
@@ -1,9 +1,10 @@
 import {rateLimiter as honoRateLimiter} from 'hono-rate-limiter';
-import type { MiddlewareHandler } from 'hono';
+import type {MiddlewareHandler} from 'hono';
 
-export const rateLimiter = (requestLimitPerMinute = 60): MiddlewareHandler => honoRateLimiter({
-  windowMs: 60 * 1000,
-  limit: requestLimitPerMinute,
-  standardHeaders: 'draft-6',
-  keyGenerator: c => c.req.header('x-forwarded-for') ?? ''
-});
+export const rateLimiter = (requestLimitPerMinute = 60): MiddlewareHandler =>
+  honoRateLimiter({
+    windowMs: 60 * 1000,
+    limit: requestLimitPerMinute,
+    standardHeaders: 'draft-6',
+    keyGenerator: c => c.req.header('x-forwarded-for') ?? ''
+  });
