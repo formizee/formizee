22a06a4d288ea27911083f42a755b904cb002b82
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-17 00:25:51 +0200

    feat(packages): create @formizee/location packages
    a package to track honojs request location info

---
+++ packages/location/src/middleware.ts
@@ -0,0 +1,23 @@
+import {getConnInfo} from '@hono/node-server/conninfo';
+import {lookup} from 'fast-geoip';
+import {Context} from 'hono';
+
+const LOOPBACK = '127.0.0.1';
+
+export const getRequestOriginCountry = async (
+  context: Context
+): Promise<string> => {
+  const connectionInfo = getConnInfo(context);
+  const requestAddress = connectionInfo.remote.address;
+
+  if (requestAddress === undefined || requestAddress === LOOPBACK) {
+    return 'Unknown';
+  }
+
+  const info = await lookup(requestAddress);
+  if (info === null) {
+    return 'Unknown';
+  }
+
+  return info.country;
+};
