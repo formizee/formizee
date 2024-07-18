33a132a740d6c795dd3ed9d25697ee5455536921
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-18 14:47:26 +0200

    fix(packages/db): remove sensitive content from types

--- packages/db/src/types.ts
+++ packages/db/src/types.ts
@@ -1,12 +1,12 @@
 import type {InferSelectModel} from 'drizzle-orm';
 import type * as schema from './schemas';
 
-export type ApiKey = InferSelectModel<typeof schema.apiKeys>;
+export type ApiKey = Omit<InferSelectModel<typeof schema.apiKeys>, "key">;
 export type AuthToken = InferSelectModel<typeof schema.authTokens>;
 export type Email = InferSelectModel<typeof schema.emails>;
 export type Endpoint = InferSelectModel<typeof schema.endpoints>;
 export type Member = InferSelectModel<typeof schema.members>;
 export type Submission = InferSelectModel<typeof schema.submissions>;
 export type Team = InferSelectModel<typeof schema.teams>;
-export type User = InferSelectModel<typeof schema.users>;
+export type User = Omit<InferSelectModel<typeof schema.users>, "password">;
 export type Waitlist = InferSelectModel<typeof schema.waitlist>;
