3180dff843d8c3bfadcf82d6a6f57a8bd137bd1e
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-19 10:24:50 +0200

    fix(apps/api): remove the integration tests

--- apps/api/tests/integration/health/status.test.ts
+++
@@ -1,9 +0,0 @@
-import {health} from '@/routes/health';
-import {describe, expect, it} from 'vitest';
-
-describe('Health /status integration', () => {
-  it('Should return 200', async () => {
-    const response = await health.request('/');
-    expect(await response.text()).toEqual('OK');
-  });
-});
