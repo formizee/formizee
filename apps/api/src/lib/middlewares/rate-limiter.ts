0b594cd00a2f2eca1be2c9bf47cf8dae306efd3f
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-21 18:58:54 +0200

    fix(apps/api): update rate-limiter to throw a standarized error

--- apps/api/src/lib/middlewares/rate-limiter.ts
+++ apps/api/src/lib/middlewares/rate-limiter.ts
@@ -1,10 +1,16 @@
 import type {MiddlewareHandler} from 'hono';
 import {rateLimiter as honoRateLimiter} from 'hono-rate-limiter';
+import {HTTPException} from 'hono/http-exception';
 
 export const rateLimiter = (requestLimitPerMinute = 60): MiddlewareHandler =>
   honoRateLimiter({
     windowMs: 60 * 1000,
     limit: requestLimitPerMinute,
     standardHeaders: 'draft-6',
-    keyGenerator: c => c.req.header('x-forwarded-for') ?? ''
+    keyGenerator: c => c.req.header('x-forwarded-for') ?? '',
+    handler: () => {
+      throw new HTTPException(429, {
+        message: 'Rate limit reached, please try again later.'
+      });
+    }
   });
