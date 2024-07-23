c14a7ba3dad72a571d44139e043d91e0cc22fe0a
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-23 15:39:21 +0200

    fix(packages/error): update exports

--- packages/error/src/index.ts
+++ packages/error/src/index.ts
@@ -1,6 +1,6 @@
 export {ErrorCodeEnum, type ErrorCode} from './utils/codes';
 export {statusToCode, codeToStatus} from './utils/index';
+export {type Result, Err, Ok} from './error-handling';
 export {SchemaError} from './models/schema';
 export {BaseError} from './models/base';
 export {HttpError} from './models/http';
-export * from './error-handling';
