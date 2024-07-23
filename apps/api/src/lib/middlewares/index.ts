2db77bce3912383ccad5cb8e274727acf9a5c308
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-23 15:39:43 +0200

    feat(apps/api): add a new middleware to access services in context

--- apps/api/src/lib/middlewares/index.ts
+++ apps/api/src/lib/middlewares/index.ts
@@ -6,6 +6,7 @@ export {prettyJSON} from 'hono/pretty-json';
 // Custom middlewares
 export {rateLimiter} from './rate-limiter';
 export {bodyLimit} from './body-limit';
+export {services} from './services';
 export {timeout} from './timeout';
 export {logger} from './logger';
 export {cors} from './cors';
