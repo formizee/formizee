9e1de16694d6ba3336565ea4a3c2ca79a1204246
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-24 20:19:36 +0200

    fix(apps/api): update v1 api router

--- apps/api/src/v1/index.ts
+++ apps/api/src/v1/index.ts
@@ -1,16 +1,19 @@
 import {newRoute} from '@/lib/hono';
 
-import {keys} from './keys';
 import {health} from './health';
-import {endpoints} from './endpoints';
+import {keys, listKeys} from './keys';
+import {endpoints, listEndpoints} from './endpoints';
 import {submissions, listSubmissions} from './submissions';
 
 const api = newRoute('/');
 
-api.route('/keys', keys);
 api.route('/status', health);
-api.route('/endpoints', endpoints);
 
+api.route('/key', keys);
+api.route('/keys', listKeys);
+
+api.route('/endpoint', endpoints);
+api.route('/endpoints', listEndpoints);
 
 api.route('/submission', submissions);
 api.route('/submissions', listSubmissions);
