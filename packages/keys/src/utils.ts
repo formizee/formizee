48785f4c112f55fa051c16e141d787a305988730
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-24 14:16:27 +0200

    style(packages/keys): format with biome

--- packages/keys/src/utils.ts
+++ packages/keys/src/utils.ts
@@ -1,5 +1,5 @@
-import { sha256 } from "@formizee/hashing";
-import { KeyV1 } from "./v1";
+import {sha256} from '@formizee/hashing';
+import {KeyV1} from './v1';
 
 export async function newKey(): Promise<{
   key: string;
@@ -7,9 +7,9 @@ export async function newKey(): Promise<{
   start: string;
 }> {
   const prefix = 'fz';
-  const key = new KeyV1({ byteLength: 13, prefix }).toString();
+  const key = new KeyV1({byteLength: 13, prefix}).toString();
   const start = key.slice(0, (prefix.length ?? 0) + 5);
   const hash = await sha256(key);
 
-  return { key, hash, start };
+  return {key, hash, start};
 }
