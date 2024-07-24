907b94d62e5b369737df5bcac397d5533addf6b4
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-24 22:27:52 +0200

    test(apps/api): update key put test

--- apps/api/src/v1/keys/put.test.ts
+++ apps/api/src/v1/keys/put.test.ts
@@ -30,6 +30,28 @@ describe('Update a key', () => {
     });
   });
 
+  it('Should get 400 on empty update', async context => {
+    const harness = await IntegrationHarness.init(context, api);
+    const {key} = await harness.createKey();
+    const {id} = harness.resources.key;
+
+    const res = await harness.put<unknown, ResponseKey>({
+      headers: {
+        authorization: `Bearer ${key}`,
+        'content-type': 'application/json'
+      },
+      url: `/key/${id}`,
+      body: {}
+    });
+
+    expect(res.status).toBe(400);
+    expect(res.body).toStrictEqual({
+      code: 'BAD_REQUEST',
+      message: "There's no fields to update",
+      docs: `${env.DOCS_URL}/api-references/errors/code/BAD_REQUEST`
+    });
+  });
+
   it('Should get 404', async context => {
     const harness = await IntegrationHarness.init(context, api);
     const {key} = await harness.createKey();
