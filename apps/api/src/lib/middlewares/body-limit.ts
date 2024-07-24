672e4803ea4db137948ae4420f2d28c8c6f96a5a
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-24 22:47:31 +0200

    fix(apps/api): update body-limit to throw a custom error

--- apps/api/src/lib/middlewares/body-limit.ts
+++ apps/api/src/lib/middlewares/body-limit.ts
@@ -1,9 +1,11 @@
-import type {Context} from 'hono';
 import {bodyLimit as honoBodyLimit} from 'hono/body-limit';
+import {HTTPException} from 'hono/http-exception';
 
 export const bodyLimit = honoBodyLimit({
   maxSize: 50 * 1024,
-  onError: (context: Context) => {
-    return context.text('The max size of the body is 50Kb.', 413);
+  onError: () => {
+    throw new HTTPException(413, {
+      message: 'The max size of the body is 50Kb.'
+    });
   }
 });
