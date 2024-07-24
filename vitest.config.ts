63db9e3334f3a69adb1d81dedca92fd4246f30ac
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-24 13:20:14 +0200

    chore: update vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,16 +5,27 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     globals: true,
+    pool: 'threads',
+    poolOptions: {
+      threads: {
+        singleThread: true
+      }
+    },
+    testTimeout: 60_000,
+    teardownTimeout: 60_000,
     coverage: {
       enabled: true,
       provider: 'v8',
       reporter: 'html',
       exclude: [
         ...configDefaults.exclude,
+        './apps/api/src/lib/testing/**',
         './packages/tailwind/**',
+        // Change this
         './packages/domain/**',
         './packages/db/**',
         './packages/ui/**',
+        './tools/local/**',
         '**/index.ts/**',
         './apps/web/**',
         '**/coverage/**',
