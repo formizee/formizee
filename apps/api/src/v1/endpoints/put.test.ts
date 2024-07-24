0ddde592e698f7eaaef75aeef4ce0430c072e84c
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-24 22:27:42 +0200

    test(apps/api): update endpoint put test

--- apps/api/src/v1/endpoints/put.test.ts
+++ apps/api/src/v1/endpoints/put.test.ts
@@ -34,6 +34,28 @@ describe('Update a endpoint', () => {
     });
   });
 
+  it('Should get 400 on empty update', async context => {
+    const harness = await IntegrationHarness.init(context, api);
+    const endpoint = harness.resources.endpoint;
+    const {key} = await harness.createKey();
+
+    const res = await harness.put<unknown, ResponseEndpoint>({
+      headers: {
+        authorization: `Bearer ${key}`,
+        'content-type': 'application/json'
+      },
+      url: `/endpoint/${endpoint.id}`,
+      body: {}
+    });
+
+    expect(res.status).toBe(400);
+    expect(res.body).toStrictEqual({
+      code: 'BAD_REQUEST',
+      message: "There's no fields to update",
+      docs: `${env.DOCS_URL}/api-references/errors/code/BAD_REQUEST`
+    });
+  });
+
   it('Should get 403 on invalid target emails', async context => {
     const harness = await IntegrationHarness.init(context, api);
     const endpoint = harness.resources.endpoint;
