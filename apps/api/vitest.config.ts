5984924ad1db36a8becce2dcb0d285b67e6f7b71
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-24 13:08:36 +0200

    chore(apps/api): update vitest configuration

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -2,5 +2,15 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()]
+  plugins: [tsconfigPaths()],
+  test: {
+    pool: 'threads',
+    poolOptions: {
+      threads: {
+        singleThread: true
+      }
+    },
+    testTimeout: 60_000,
+    teardownTimeout: 60_000
+  }
 });
