4163d5bb5a07087c22fa3ab282a8441c9223c9c3
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-26 12:14:34 +0200

    test(apps/api): fix import statement of the logger middleware

--- apps/api/src/lib/middlewares/logger/service.test.ts
+++ apps/api/src/lib/middlewares/logger/service.test.ts
@@ -1,5 +1,5 @@
 import {describe, it, expect, vi, beforeEach, afterEach} from 'vitest';
-import {logger} from './logger';
+import {logger} from './service';
 import {Hono} from 'hono';
 
 // Utility to mock console.info
