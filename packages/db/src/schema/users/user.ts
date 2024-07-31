b7c1998da4dd286d734fe70572941dae966ca1ea
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-07-31 12:48:05 +0200

    feat(packages/db): add next auth tables

--- packages/db/src/schema/users/user.ts
+++ packages/db/src/schema/users/user.ts
@@ -1,14 +1,18 @@
 import {
   text,
-  boolean,
-  timestamp,
   unique,
+  pgEnum,
   pgTable,
-  pgEnum
+  boolean,
+  integer,
+  timestamp,
+  primaryKey
 } from 'drizzle-orm/pg-core';
 import {workspace, workspaceRole} from '../workspaces';
-import {relations} from 'drizzle-orm';
+// @ts-ignore
+import type {AdapterAccount} from 'next-auth/adapters';
 import {memberPermissions} from './constants';
+import {relations} from 'drizzle-orm';
 
 export const user = pgTable(
   'users',
@@ -16,9 +20,10 @@ export const user = pgTable(
     id: text('id').primaryKey(),
     name: text('name').notNull(),
     slug: text('slug').notNull().unique(),
+    image: text('image'),
 
     email: text('email').notNull().unique(),
-    isVerified: boolean('is_verified').notNull().default(false),
+    emailVerified: timestamp('email_verified', {mode: 'date'}),
 
     lastAccess: timestamp('last_access').notNull().defaultNow(),
     createdAt: timestamp('created_at').notNull().defaultNow(),
@@ -105,3 +110,58 @@ export const usersToEmailsRelations = relations(usersToEmails, ({one}) => ({
     references: [user.id]
   })
 }));
+
+// Auth.js //
+
+export const account = pgTable(
+  'accounts',
+  {
+    userId: text('user_id')
+      .notNull()
+      .references(() => user.id, {onDelete: 'cascade'}),
+    type: text('type').$type<AdapterAccount['type']>().notNull(),
+    provider: text('provider').notNull(),
+    providerAccountId: text('provider_account_id').notNull(),
+    refresh_token: text('refresh_token'),
+    access_token: text('access_token'),
+    expires_at: integer('expires_at'),
+    token_type: text('token_type'),
+    scope: text('scope'),
+    id_token: text('id_token'),
+    session_state: text('session_state')
+  },
+  account => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId]
+    })
+  })
+);
+
+export const session = pgTable('sessions', {
+  sessionToken: text('session_token').primaryKey(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id, {onDelete: 'cascade'}),
+  expires: timestamp('expires', {mode: 'date'}).notNull()
+});
+
+export const authenticator = pgTable(
+  'authenticators',
+  {
+    credentialID: text('credential_id').notNull().unique(),
+    userId: text('user_id')
+      .notNull()
+      .references(() => user.id, {onDelete: 'cascade'}),
+    providerAccountId: text('provider_account_id').notNull(),
+    credentialPublicKey: text('credential_public_key').notNull(),
+    counter: integer('counter').notNull(),
+    credentialDeviceType: text('credential_device_type').notNull(),
+    credentialBackedUp: boolean('credential_backed_up').notNull(),
+    transports: text('transports')
+  },
+  authenticator => ({
+    compositePK: primaryKey({
+      columns: [authenticator.userId, authenticator.credentialID]
+    })
+  })
+);
