c5ec8ba7d45d7a1ccf2e9d6cc8990014e6541fec
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-08-06 14:01:11 +0200

    feat(apps/dashboard): add the transition component

---
+++ apps/dashboard/src/components/transition/index.tsx
@@ -0,0 +1,16 @@
+import {cn} from '@formizee/ui';
+
+interface TransitionProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export function Transition(props: TransitionProps): JSX.Element {
+  return (
+    <div className={cn('animate-fade-in', props.className)}>
+      {props.children}
+    </div>
+  );
+}
+
+export default Transition;
