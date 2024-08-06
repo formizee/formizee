e7c193045fc0a37f6ba4889931e4f65c33f90bbb
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-08-06 15:10:32 +0200

    feat(packages/ui): add the book icon

---
+++ packages/ui/src/icons/book.tsx
@@ -0,0 +1,14 @@
+import { cn } from '../lib/ui';
+import type { IconProps } from './types';
+
+export function BookIcon(props: IconProps): JSX.Element {
+  return (
+    <svg
+      className={cn('h-4 w-4', props.className)}
+      fill="currentColor"
+      viewBox="0 0 16 16"
+    >
+      <path d="M7.25 3.688a8.035 8.035 0 0 0-4.872-.523A.48.48 0 0 0 2 3.64v7.994c0 .345.342.588.679.512a6.02 6.02 0 0 1 4.571.81V3.688ZM8.75 12.956a6.02 6.02 0 0 1 4.571-.81c.337.075.679-.167.679-.512V3.64a.48.48 0 0 0-.378-.475 8.034 8.034 0 0 0-4.872.523v9.268Z" />
+    </svg>
+  );
+}
--- packages/ui/src/icons/index.tsx
+++ packages/ui/src/icons/index.tsx
@@ -17,5 +17,6 @@ export {LinkIcon} from './link';
 export {UserIcon} from './user';
 export {MailIcon} from './mail';
 export {HomeIcon} from './home';
+export {BookIcon} from './book';
 export {MoonIcon} from './moon';
 export {SunIcon} from './sun';
