502696c9f1fd465efc395add0672b0d778add700
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-08-06 15:09:23 +0200

    feat(apps/dashboard): create a heading component

--- apps/dashboard/src/components/index.ts
+++ apps/dashboard/src/components/index.ts
@@ -1,4 +1,4 @@
+export {NavbarOptions} from './navbar-options';
 export {IconPicker} from './icon-picker';
 export {Transition} from './transition';
-export {Sidebar} from './sidebar';
-export {Navbar} from './navbar';
+export {Heading} from './heading';
