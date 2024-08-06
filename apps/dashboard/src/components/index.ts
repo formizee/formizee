840c5607da3876fe8942c7399bc8eacaffb57043
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-08-06 23:36:50 +0200

    feat(apps/dashboard): add footer component

--- apps/dashboard/src/components/index.ts
+++ apps/dashboard/src/components/index.ts
@@ -2,3 +2,4 @@ export {NavbarOptions} from './navbar-options';
 export {IconPicker} from './icon-picker';
 export {Transition} from './transition';
 export {Heading} from './heading';
+export {Footer} from './footer';
