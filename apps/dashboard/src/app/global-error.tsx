588d9346df3b130f3084ac45e76846bad37b24ec
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-08-09 19:44:13 +0200

    fix(apps/dashboard): update error pages

--- apps/dashboard/src/app/global-error.tsx
+++ apps/dashboard/src/app/global-error.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import {UndoIcon} from '@formizee/ui/icons';
-import logo from '@/../public/logo.svg';
 import {Transition} from '@/components';
+import logo from '@/../public/logo.svg';
 import {Button} from '@formizee/ui';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -30,11 +30,11 @@ export default function GlobalError({
           </h1>
         </header>
         <section className="flex flex-col gap-4 items-center max-w-[500px]">
-          <p className="max-w-96 text-balance text-center sm:text-wrap sm:text-start">
+          <p className="max-w-96 text-neutral-600 dark:text-neutral-400 text-balance text-center sm:text-wrap sm:text-start">
             It seems like something unexpected happened. We&apos;re looking into
             it right now.
           </p>
-          <code className="rounded-md bg-red-800/50 px-2 py-1 font-mono text-red-200 text-sm">
+          <code className="rounded-md bg-red-200 dark:bg-red-800/50 px-2 py-1 font-mono text-neutral-800 dark:text-red-200 text-sm">
             {error.message}
           </code>
           <Button onClick={reset} className="group mt-4 w-full max-w-96">
