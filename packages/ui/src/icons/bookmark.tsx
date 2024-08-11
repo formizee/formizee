a40d023ed77038a13e3508ddd48ea68d7d06cffe
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-08-11 21:24:53 +0200

    feat(packages/ui): add a lot of icons!!!

---
+++ packages/ui/src/icons/bookmark.tsx
@@ -0,0 +1,14 @@
+import { cn } from '../lib/ui';
+import type { IconProps } from './types';
+
+export function BookmarkIcon(props: IconProps): JSX.Element {
+  return (
+    <svg
+      className={cn('h-4 w-4', props.className)}
+      fill="currentColor"
+      viewBox="0 0 16 16"
+    >
+      <path d="M3.75 2a.75.75 0 0 0-.75.75v10.5a.75.75 0 0 0 1.28.53L8 10.06l3.72 3.72a.75.75 0 0 0 1.28-.53V2.75a.75.75 0 0 0-.75-.75h-8.5Z" />
+    </svg>
+  );
+}
---
+++ packages/ui/src/icons/paint.tsx
@@ -0,0 +1,14 @@
+import { cn } from '../lib/ui';
+import type { IconProps } from './types';
+
+export function PaintIcon(props: IconProps): JSX.Element {
+  return (
+    <svg
+      className={cn('h-4 w-4', props.className)}
+      fill="currentColor"
+      viewBox="0 0 16 16"
+    >
+      <path d="M12.613 1.258a1.535 1.535 0 0 1 2.13 2.129l-1.905 2.856a8 8 0 0 1-3.56 2.939 4.011 4.011 0 0 0-2.46-2.46 8 8 0 0 1 2.94-3.56l2.855-1.904ZM5.5 8A2.5 2.5 0 0 0 3 10.5a.5.5 0 0 1-.7.459.75.75 0 0 0-.983 1A3.5 3.5 0 0 0 8 10.5 2.5 2.5 0 0 0 5.5 8Z" />
+    </svg>
+  );
+}
