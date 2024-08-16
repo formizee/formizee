6797a16a3745d3ffbc7faf8b194ec4f5497ef22e
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-08-16 19:14:01 +0200

    fix(apps/dashboard): update components export barrel

--- apps/dashboard/src/components/index.ts
+++ apps/dashboard/src/components/index.ts
@@ -3,4 +3,5 @@ export {IconPicker} from './icon-picker';
 export {Transition} from './transition';
 export {Heading} from './heading';
 export {Footer} from './footer';
+export {Icon} from './icon';
 export * from './settings-card';
