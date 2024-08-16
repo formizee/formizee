06248f249688fbf094c85dc39822f908077f138b
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-08-16 19:13:23 +0200

    feat(apps/dashboard): create the icon component and finish the picker

--- apps/dashboard/src/components/icon-picker/index.tsx
+++ apps/dashboard/src/components/icon-picker/index.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import {PaintIcon, StarIcon} from '@formizee/ui/icons';
+import {Icon, getColor} from '../icon';
+import Transition from '../transition';
 import {schema} from '@formizee/db';
-import {useState} from 'react';
+import {api} from '@/trpc/client';
 
 import {
   Button,
@@ -12,91 +15,150 @@ import {
   TabsTrigger,
   TabsContent,
   TabsList,
-  cn
+  cn,
+  toast
 } from '@formizee/ui';
-
-import {DocumentIcon} from '@formizee/ui/icons';
 
 type EndpointIcon = (typeof schema.endpointIcon)[number];
 type EndpointColor = (typeof schema.endpointColor)[number];
 
-const getColor = (color: EndpointColor) => {
-  switch (color) {
-    case 'red':
-      return 'fill-red-400';
-    case 'lime':
-      return 'fill-lime-400';
-    case 'teal':
-      return 'fill-teal-400';
-    case 'cyan':
-      return 'fill-cyan-400';
-    case 'pink':
-      return 'fill-pink-400';
-    case 'amber':
-      return 'fill-amber-400';
-    case 'indigo':
-      return 'fill-indigo-400';
-    case 'violet':
-      return 'fill-violet-400';
-    case 'white':
-      return 'fill-neutral-950 dark:fill-neutral-50';
-    default:
-      return 'fill-neutral-400';
-  }
-};
+interface Props {
+  workspaceSlug: string;
+  endpointSlug: string;
+}
 
-const Icon = (props: {icon: EndpointIcon; color: EndpointColor}) => {
-  switch (props.icon) {
-    default:
-      return <DocumentIcon className={getColor(props.color)} />;
-  }
-};
+export const IconPicker = (props: Props) => {
+  const {data} = api.endpoint.getBySlug.useQuery(props, {
+    refetchOnWindowFocus: false
+  });
+  const endpoint = data ?? null;
 
-export const IconPicker = () => {
-  const [currentIcon, _setCurrentIcon] = useState<EndpointIcon>('file');
-  const [currentColor, _setCurrentColor] = useState<EndpointColor>('gray');
+  const utils = api.useUtils();
+
+  const updateEndpoint = api.endpoint.update.useMutation({
+    onError: error => {
+      toast({
+        variant: 'destructive',
+        title: 'Error updating the icon',
+        description: error.message
+      });
+    },
+    onSuccess: async newEndpoint => {
+      utils.endpoint.getBySlug.setData(props, newEndpoint);
+      const list = utils.endpoint.list.getData({
+        workspaceSlug: props.workspaceSlug
+      });
+
+      if (!list) {
+        utils.endpoint.list.invalidate();
+        return;
+      }
+
+      const updatedList = list.map(endpoint =>
+        endpoint.id === newEndpoint.id ? newEndpoint : endpoint
+      );
+
+      utils.endpoint.list.setData(
+        {workspaceSlug: props.workspaceSlug},
+        updatedList
+      );
+    }
+  });
+
+  const updateColor = (color: EndpointColor) => {
+    if (!endpoint) {
+      return;
+    }
+
+    updateEndpoint.mutate({id: endpoint.id, color});
+  };
+
+  const updateIcon = (icon: EndpointIcon) => {
+    if (!endpoint) {
+      return;
+    }
+
+    updateEndpoint.mutate({id: endpoint.id, icon});
+  };
 
   return (
     <div className="flex flex-col">
       <Popover>
         <PopoverTrigger asChild>
-          <Button variant="outline" size="icon">
-            <Icon icon={currentIcon} color={currentColor} />
+          <Button variant="outline" size="icon" className="size-12">
+            {endpoint !== null ? (
+              <Icon
+                icon={endpoint.icon}
+                color={endpoint.color}
+                selected={true}
+                className="size-8 animate-fade-in"
+              />
+            ) : (
+              <></>
+            )}
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="bg-neutral-900 border border-neutral-700">
-          <Tabs defaultValue="icons">
-            <TabsList>
-              <TabsTrigger value="icons">Icons</TabsTrigger>
-              <TabsTrigger value="colors">Colors</TabsTrigger>
-            </TabsList>
-            <TabsContent value="icons">
-              <div className="grid grid-cols-5">
-                {schema.endpointIcon.map(icon => {
-                  return (
-                    <Button key={icon} variant="ghost" size="icon">
-                      <Icon icon={icon} color="gray" />
-                    </Button>
-                  );
-                })}
-              </div>
-            </TabsContent>
-            <TabsContent value="colors">
-              <div className="grid grid-cols-5">
-                {schema.endpointColor.map(color => {
-                  return (
-                    <Button key={color} variant="ghost" size="icon">
-                      <svg className="size-4">
-                        <rect
-                          className={cn(getColor(color), 'size-4 rounded-md')}
-                        />
-                      </svg>
-                    </Button>
-                  );
-                })}
-              </div>
-            </TabsContent>
-          </Tabs>
+        <PopoverContent
+          align="start"
+          sideOffset={8}
+          className="bg-neutral-100 dark:bg-neutral-900 border border-neutral-200 dark:border-neutral-700"
+        >
+          <Transition>
+            <Tabs defaultValue="icons">
+              <TabsList className="w-full mb-2">
+                <TabsTrigger value="icons">
+                  <StarIcon />
+                  Icons
+                </TabsTrigger>
+                <TabsTrigger value="colors">
+                  <PaintIcon />
+                  Colors
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="icons">
+                <Transition className="grid grid-cols-5">
+                  {schema.endpointIcon.map(icon => {
+                    return (
+                      <Button
+                        key={icon}
+                        size="icon"
+                        onClick={() => updateIcon(icon)}
+                        variant={endpoint?.icon !== icon ? 'ghost' : 'outline'}
+                      >
+                        <Icon icon={icon} color="gray" selected={true} />
+                      </Button>
+                    );
+                  })}
+                </Transition>
+              </TabsContent>
+              <TabsContent value="colors">
+                <Transition className="grid grid-cols-5">
+                  {schema.endpointColor.map(color => {
+                    return (
+                      <Button
+                        key={color}
+                        size="icon"
+                        onClick={() => updateColor(color)}
+                        variant={
+                          endpoint?.color !== color ? 'ghost' : 'outline'
+                        }
+                      >
+                        <svg className="size-4">
+                          <rect
+                            rx={5}
+                            className={cn(
+                              getColor(color, true),
+                              'size-4 rounded-md'
+                            )}
+                          />
+                        </svg>
+                      </Button>
+                    );
+                  })}
+                </Transition>
+              </TabsContent>
+            </Tabs>
+          </Transition>
         </PopoverContent>
       </Popover>
     </div>
