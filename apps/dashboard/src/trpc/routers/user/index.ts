de20d8f2d2c6422e996c424090d0a4a17d278ee6
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-08-19 19:29:41 +0200

    refactor(apps/dashboard): separate user procedures

--- apps/dashboard/src/trpc/routers/user/index.ts
+++ apps/dashboard/src/trpc/routers/user/index.ts
@@ -1,27 +1,6 @@
-import {router, protectedProcedure} from '@/trpc';
-import {TRPCError} from '@trpc/server';
-import {database} from '@/lib/db';
-import {z} from 'zod';
+import {router} from '@/trpc';
+import {getUser} from './get';
 
 export const userRouter = router({
-  get: protectedProcedure
-    .input(
-      z.object({
-        id: z.string()
-      })
-    )
-    .query(async ({input}) => {
-      const user = await database.query.user.findFirst({
-        where: (table, {eq}) => eq(table.id, input.id)
-      });
-
-      if (!user) {
-        throw new TRPCError({
-          code: 'NOT_FOUND',
-          message: 'User not found.'
-        });
-      }
-
-      return user;
-    })
+  get: getUser
 });
