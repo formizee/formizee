c1e062b3363202c1677b79e9b82b60c66a7fd9e8
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-08-19 19:30:05 +0200

    feat(apps/dashboard): add getMembers procedure to workspace

---
+++ apps/dashboard/src/trpc/routers/workspace/get-members.ts
@@ -0,0 +1,45 @@
+import {protectedProcedure} from '@/trpc';
+import {TRPCError} from '@trpc/server';
+import {database} from '@/lib/db';
+import {z} from 'zod';
+
+export const getWorkspaceMembers = protectedProcedure
+  .input(
+    z.object({
+      slug: z.string().max(64)
+    })
+  )
+  .query(async ({input, ctx}) => {
+    const workspace = await database.query.workspace.findFirst({
+      where: (table, {eq}) => eq(table.slug, input.slug)
+    });
+
+    if (!workspace) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'Workspace not found.'
+      });
+    }
+
+    const authorized = await database.query.usersToWorkspaces.findFirst({
+      where: (table, {and, eq}) =>
+        and(
+          eq(table.userId, ctx.user.id ?? ''),
+          eq(table.workspaceId, workspace.id)
+        )
+    });
+
+    if (!authorized) {
+      throw new TRPCError({
+        code: 'UNAUTHORIZED',
+        message: 'You do not have permission to view this workspace.'
+      });
+    }
+
+    const members = await database.query.usersToWorkspaces.findMany({
+      where: (table, {eq}) => eq(table.workspaceId, workspace.id),
+      with: {user: true}
+    });
+
+    return members;
+  });
--- apps/dashboard/src/trpc/routers/workspace/index.ts
+++ apps/dashboard/src/trpc/routers/workspace/index.ts
@@ -2,7 +2,9 @@ import {router} from '@/trpc';
 //
 //Routes
 import {getWorkspaceBySlug} from './get';
+import {getWorkspaceMembers} from './get-members';
 
 export const workspaceRouter = router({
-  getBySlug: getWorkspaceBySlug
+  getBySlug: getWorkspaceBySlug,
+  getMembers: getWorkspaceMembers
 });
