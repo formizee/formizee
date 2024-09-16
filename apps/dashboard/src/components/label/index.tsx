6c6045a59eeb7b982d9f116fdb26db2c9a4d3bde
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-09-16 11:33:17 +0200

    fix(apps/dashboard): update label active styles

--- apps/dashboard/src/components/label/index.tsx
+++ apps/dashboard/src/components/label/index.tsx
@@ -8,7 +8,7 @@ interface LabelProps {
 
 export const Label = (props: LabelProps) => {
   const activeStyle =
-    'border-green-500 dark:border-green-400 bg-green-400 dark:bg-green-500  text-neutral-50 dark:text-neutral-950';
+    'border-green-500 dark:border-green-700 bg-green-100 dark:bg-green-950  text-neutral-700 dark:text-neutral-200';
   const pausedStyle =
     'border-yellow-500 dark:border-yellow-400 bg-yellow-400 dark:bg-yellow-500  text-neutral-50 dark:text-neutral-950';
   const variant = props.variant === 'active' ? activeStyle : pausedStyle;
