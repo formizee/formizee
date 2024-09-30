653e4ef9ed2b280eee0172407763accfe53b06ff
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-09-30 13:26:58 +0200

    fix(apps/dashboard): update integrations content style

--- apps/dashboard/src/app/[workspaceSlug]/[endpointSlug]/(content)/integrations.tsx
+++ apps/dashboard/src/app/[workspaceSlug]/[endpointSlug]/(content)/integrations.tsx
@@ -4,7 +4,7 @@ export const Integrations = () => {
   return (
     <Transition className="flex flex-col py-6 gap-6">
       <div className="flex flex-col items-center justify-center rounded-md border border-dashed border-neutral-200 dark:border-neutral-800 h-96">
-        <h2 className="font-semibold text-2xl px-6 pt-6">Coming Soon</h2>
+        <h2 className="font-semibold text-xl px-6 pt-6">Coming Soon...</h2>
       </div>
     </Transition>
   );
