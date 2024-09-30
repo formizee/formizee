536aef1b896372ca6ec7ea883e87bb6e4bf49651
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-09-30 13:30:32 +0200

    feat(apps/dashboard): add the member dialogs

---
+++ apps/dashboard/src/app/[workspaceSlug]/settings/_components/dialogs/member/new.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import {PlusIcon} from '@formizee/ui/icons';
+import {Button} from '@formizee/ui';
+
+export const AddMemberButton = () => (
+  <Button disabled>
+    Add New Member
+    <PlusIcon />
+  </Button>
+);
