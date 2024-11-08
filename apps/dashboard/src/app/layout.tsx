4fc41367486e773ab21e4a884763f0aa870b3bc1
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-08 16:05:13 +0100

    feat(apps/dashboard): add vercel analytics

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import {Analytics} from '@vercel/analytics/react';
 import {ThemeProvider} from '@/components/theme';
 import {Toaster} from '@formizee/ui/toaster';
 import {Inter} from 'next/font/google';
@@ -51,6 +52,7 @@ export default function RootLayout({
             <Toaster />
           </TrpcProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
