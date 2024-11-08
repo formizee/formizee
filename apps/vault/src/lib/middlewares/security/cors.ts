c6ba475fcd7c0b29893bbb30ceaef5e0da287f92
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-08 19:08:20 +0100

    fix(apps/vault): update the cors policy to * (testing)

--- apps/vault/src/lib/middlewares/security/cors.ts
+++ apps/vault/src/lib/middlewares/security/cors.ts
@@ -5,11 +5,7 @@ import {cors as honoCors} from 'hono/cors';
 export const cors = (): MiddlewareHandler<HonoEnv> => {
   return async function handler(_context, next) {
     honoCors({
-      origin: origin => {
-        const allowedOrigins =
-          /^(https?:\/\/)?((.*\.)?formizee\.com|localhost:3001)$/;
-        return allowedOrigins.test(origin) ? origin : null;
-      },
+      origin: "*",
       allowMethods: ['GET', 'POST', 'DELETE'],
       credentials: true
     });
