296d9eb54ab806d8d652904570c1a718a2bc85f4
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-08 15:07:15 +0100

    fix(apps/dashboard): update enviroment type validation for deploy

--- apps/dashboard/src/lib/enviroment/index.ts
+++ apps/dashboard/src/lib/enviroment/index.ts
@@ -11,7 +11,7 @@ export const env = () =>
       VERCEL_URL: z.string().optional(),
 
       // Databases
-      DATABASE_URL: z.string(),
+      DATABASE_URL: z.string().url().default("postgresql://formizee:password@localhost/formizee"),
 
       // Analytics
       TINYBIRD_URL: z.string().url().optional(),
