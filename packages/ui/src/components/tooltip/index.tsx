c3895e0488ddf1765c5fdea7f997cac849b92923
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-12 18:58:36 +0100

    feat(packages/ui): add sidebar and collapsible components

--- packages/ui/src/components/tooltip/index.tsx
+++ packages/ui/src/components/tooltip/index.tsx
@@ -4,31 +4,28 @@ import * as React from 'react';
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import {cn} from '../../lib/ui';
 
-function Tooltip(props: {children: React.ReactNode}): JSX.Element {
-  return (
-    <TooltipPrimitive.Provider delayDuration={300}>
-      <TooltipPrimitive.Root>{props.children}</TooltipPrimitive.Root>
-    </TooltipPrimitive.Provider>
-  );
-}
+const TooltipProvider = TooltipPrimitive.Provider
 
-const TooltipTrigger = TooltipPrimitive.Trigger;
+const Tooltip = TooltipPrimitive.Root
+
+const TooltipTrigger = TooltipPrimitive.Trigger
 
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
->(({className, sideOffset = 4, ...props}, ref) => (
-  <TooltipPrimitive.Content
-    ref={ref}
-    avoidCollisions
-    sideOffset={sideOffset}
-    className={cn(
-      'animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 overflow-hidden rounded-md border bg-neutral-200 px-3 py-1.5 text-xs text-neutral-950 dark:border-neutral-800 dark:bg-neutral-900/80 dark:text-neutral-100 dark:backdrop-blur-sm',
-      className
-    )}
-    {...props}
-  />
-));
-TooltipContent.displayName = TooltipPrimitive.Content.displayName;
+>(({ className, sideOffset = 4, ...props }, ref) => (
+  <TooltipPrimitive.Portal>
+    <TooltipPrimitive.Content
+      ref={ref}
+      sideOffset={sideOffset}
+      className={cn(
+        "z-50 overflow-hidden rounded-md bg-neutral-900 px-3 py-1.5 text-xs text-neutral-50 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:bg-neutral-50 dark:text-neutral-900",
+        className
+      )}
+      {...props}
+    />
+  </TooltipPrimitive.Portal>
+))
+TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
-export {Tooltip, TooltipTrigger, TooltipContent};
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
