a9ca8658d244f97da7cb8e0c02b0df6fa272f7fa
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-12 22:00:23 +0100

    feat(packages/ui): add new icon outline variants

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -7,6 +7,7 @@ export * from './components/button';
 export * from './components/card';
 export * from './components/code-block';
 export * from './components/collapsible';
+export * from './components/command';
 export * from './components/dialog';
 export * from './components/dropdown-menu';
 export * from './components/input';
