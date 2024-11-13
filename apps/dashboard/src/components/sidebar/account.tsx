be3cbbd0877d2581761863ef332c639c8da101ad
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-13 10:36:27 +0100

    fix(apps/dashboard): add skeleton to the account button

--- apps/dashboard/src/components/sidebar/account.tsx
+++ apps/dashboard/src/components/sidebar/account.tsx
@@ -14,7 +14,8 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger
+  DropdownMenuTrigger,
+  Skeleton
 } from '@formizee/ui';
 import {
   BookIcon,
@@ -32,9 +33,21 @@ interface AccountProps {
 }
 
 export const Account = (props: AccountProps) => {
-  const user = api.user.get.useQuery({id: props.userId}).data;
+  const {data, isLoading} = api.user.get.useQuery({id: props.userId});
 
-  if (!user) {
+  if (isLoading) {
+    return (
+      <div className="animate-fade-in flex gap-2 m-4">
+        <Skeleton className="px-4 size-8" />
+        <div className="flex flex-col gap-2">
+          <Skeleton className="w-40 mr-2 h-4" />
+          <Skeleton className="w-24 mr-2 h-2" />
+        </div>
+      </div>
+    );
+  }
+
+  if (!data) {
     return;
   }
 
@@ -44,18 +57,18 @@ export const Account = (props: AccountProps) => {
         <SidebarMenuItem>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <SidebarMenuButton className="h-10 mb-2">
-                <div className="flex items-center justify-center size-8 rounded-md bg-neutral-600 dark:bg-neutral-400">
+              <SidebarMenuButton className="h-12">
+                <div className="flex flex-row items-center px-4 justify-center size-8 rounded-md bg-neutral-600 dark:bg-neutral-400">
                   <span className="fixed text-lg text-neutral-50 dark:text-neutral-950 font-semibold">
-                    {user.name.split('')[0]?.toUpperCase()}
+                    {data.name.split('')[0]?.toUpperCase()}
                   </span>
                 </div>
-                <div className="flex flex-col">
-                  <span className="text-sm font-regular max-w-[9rem] truncate mr-2">
-                    {user.name}
+                <div className="flex flex-col max-w-48">
+                  <span className="text-sm font-regular truncate mr-2">
+                    {data.name}
                   </span>
                   <span className="text-xs text-neutral-600 dark:text-neutral-400 font-regular max-w-[12.5rem] truncate">
-                    {user.slug}
+                    {data.slug}
                   </span>
                 </div>
                 <ChevronUpDownIcon className="w-8 h-8" />
@@ -63,17 +76,17 @@ export const Account = (props: AccountProps) => {
             </DropdownMenuTrigger>
             <DropdownMenuContent side="right" align="end">
               <DropdownMenuLabel className="flex flex-row gap-2">
-                <div className="flex items-center justify-center size-10 rounded-md bg-neutral-600 dark:bg-neutral-400">
+                <div className="flex select-none items-center justify-center size-10 rounded-md bg-neutral-600 dark:bg-neutral-400">
                   <span className="fixed text-lg text-neutral-50 dark:text-neutral-950 font-semibold">
-                    {user.name.split('')[0]?.toUpperCase()}
+                    {data.name.split('')[0]?.toUpperCase()}
                   </span>
                 </div>
                 <div className="flex flex-col">
                   <span className="text-sm font-regular max-w-[15rem] truncate mr-2">
-                    {user.name}
+                    {data.name}
                   </span>
                   <span className="text-xs text-neutral-600 dark:text-neutral-400 font-regular max-w-[12.5rem] truncate mt-0.5">
-                    Free Plan
+                    {data.slug}
                   </span>
                 </div>
               </DropdownMenuLabel>
