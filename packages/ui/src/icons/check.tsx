7797ab75632eda3e5b3a17a4b96371b105f1c4f8
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-13 10:41:54 +0100

    fix(packages/ui): update the check-icon props

--- packages/ui/src/icons/check.tsx
+++ packages/ui/src/icons/check.tsx
@@ -2,7 +2,7 @@ import {cn} from '../lib/ui';
 import type {IconProps} from './types';
 
 interface CheckIconProps extends IconProps {
-  variant?: 'default' | 'circle';
+  variant?: 'default' | 'outline';
 }
 
 export function CheckIcon(props: CheckIconProps): JSX.Element {
@@ -12,7 +12,7 @@ export function CheckIcon(props: CheckIconProps): JSX.Element {
       fill="currentColor"
       viewBox="0 0 16 16"
     >
-      {props.variant === 'circle' ? (
+      {props.variant === 'outline' ? (
         <path
           clipRule="evenodd"
           d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm3.844-8.791a.75.75 0 0 0-1.188-.918l-3.7 4.79-1.649-1.833a.75.75 0 1 0-1.114 1.004l2.25 2.5a.75.75 0 0 0 1.15-.043l4.25-5.5Z"
