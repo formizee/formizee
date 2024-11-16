c33becdf83a799cc1d5646b89e20ee1fde825cd5
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-16 14:45:56 +0100

    feat(packages/plans): add a utility to calculate billing cycles

--- packages/plans/src/index.ts
+++ packages/plans/src/index.ts
@@ -1,4 +1,9 @@
-export {getLimit, getLimits, getPlanConfig} from './utils';
+export {
+  getLimit,
+  getLimits,
+  getPlanConfig,
+  calculatePlanCycleDates
+} from './utils';
 export * from './pricing';
 export * from './config';
 export * from './types';
