ce37bef28f19478dcc3a9052c7330827ad12e6f6
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-18 17:32:36 +0100

    fix(apps/dashboard): update the table search name due to issues

--- apps/dashboard/src/components/table/options.tsx
+++ apps/dashboard/src/components/table/options.tsx
@@ -69,6 +69,7 @@ export function TableSearchOptions<TData>(
   return (
     <Input
       type="search"
+      name="search"
       placeholder={props.placeholder ?? 'Search...'}
       value={
         (props.table.getColumn(props.column)?.getFilterValue() as string) ?? ''
