cdd13d6f8625b072da56ff6072859abaf49c92bc
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-19 01:45:28 +0100

    fix(apps/dashboard): forget to commit the components export barrel

--- apps/dashboard/src/components/index.ts
+++ apps/dashboard/src/components/index.ts
@@ -3,6 +3,7 @@ export {IconPicker} from './icon/picker';
 export {Transition} from './transition';
 export {Footer} from './footer';
 export {Label} from './label';
+export {Badge} from './badge';
 export {Icon} from './icon';
 
 export * from './settings-card';
