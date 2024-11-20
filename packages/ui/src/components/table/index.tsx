ac18734a69db8028af886b851e800caae834a637
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-20 23:18:11 +0100

    fix(packages/ui): update table overflow styles

--- packages/ui/src/components/table/index.tsx
+++ packages/ui/src/components/table/index.tsx
@@ -5,7 +5,7 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
+  <div className="relative w-full overflow-auto overflow-light-style dark:overflow-dark-style">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
