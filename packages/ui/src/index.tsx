27685d9b9f51b96e63b2f648f01437c8fdcbffbf
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-21 19:52:25 +0100

    fix(packages/ui): remove unused components

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,7 +2,6 @@ import './styles/globals.css';
 
 export {cn} from './lib/ui';
 
-export * from './components/badge';
 export * from './components/breadcrumb';
 export * from './components/button';
 export * from './components/card';
@@ -22,7 +21,6 @@ export * from './components/separator';
 export * from './components/sheet';
 export * from './components/skeleton';
 export * from './components/table';
-export * from './components/tabs';
 export * from './components/text-area';
 export * from './components/toast';
 export * from './components/tooltip';
