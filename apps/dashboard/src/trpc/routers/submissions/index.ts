e8b6293865ebfc0c14a12a039462a2085307efa0
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-21 14:46:22 +0100

    feat(apps/dashboard): add the delete group procedure for submissions

--- apps/dashboard/src/trpc/routers/submissions/index.ts
+++ apps/dashboard/src/trpc/routers/submissions/index.ts
@@ -1,9 +1,11 @@
 import {router} from '@/trpc';
 
+import {deleteSubmissionGroup} from './delete-group';
 import {deleteSubmission} from './delete';
 import {listSubmissions} from './list';
 
 export const submissionsRouter = router({
+  deleteGroup: deleteSubmissionGroup,
   delete: deleteSubmission,
   list: listSubmissions
 });
