048735ef42cf5e1f0fa9e5de959e92a2c432c827
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-27 10:28:15 +0100

    fix(apps/vault): update enviroment

--- apps/vault/src/lib/enviroment/index.ts
+++ apps/vault/src/lib/enviroment/index.ts
@@ -4,26 +4,26 @@ import {z} from 'zod';
 export const zEnv = z.object({
   // General
   ENVIROMENT: z.enum(['test', 'preview', 'development', 'production']),
+  DOCS_URL: z.string().url().default('https://docs.formizee.com'),
   VERSION: z.string().default(project.version),
-  DOCS_URL: z.string().url(),
-
-  // Analytics
-  TINYBIRD_URL: z.string().url().optional(),
-  TINYBIRD_TOKEN: z.string().optional(),
 
   //Cloudflare Bindings
   cache: z.custom<KVNamespace>(),
   keys: z.custom<KVNamespace>(),
 
-  // Database
-  DATABASE_URL: z.string().url().default('http://locahost:8081'),
+  // Databases
+  DATABASE_URL: z.string().url().default('http://localhost:8080'),
   DATABASE_AUTH_TOKEN: z.string().optional(),
 
   // Storage
-  STORAGE_ACCESS_KEY_ID: z.string().default('minio_root_user'),
   STORAGE_SECRET_ACCESS_KEY: z.string().default('minio_root_password'),
   STORAGE_ENDPOINT: z.string().url().default('http://localhost:3902'),
+  STORAGE_ACCESS_KEY_ID: z.string().default('minio_root_user'),
   STORAGE_BUCKET: z.string().default('storage'),
+
+  // Analytics
+  TINYBIRD_URL: z.string().url().optional(),
+  TINYBIRD_TOKEN: z.string().optional(),
 
   /* --- Secrets --- */
   // Used to encrypt the dek's that encrypts submissions
