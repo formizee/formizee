147ad8c76b1e7e50ca1f23938bebc1b9269f1608
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-28 18:48:41 +0100

    style: format code with biome

--- tools/local/src/db.ts
+++ tools/local/src/db.ts
@@ -6,17 +6,27 @@ import {task} from './util';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export async function prepareDatabase(database: 'main' | 'submissions'): Promise<void> {
-  if(database === 'main') {
-    await migrateTables(database, "http://localhost:8080", 'pnpm push:main');
+export async function prepareDatabase(
+  database: 'main' | 'submissions'
+): Promise<void> {
+  if (database === 'main') {
+    await migrateTables(database, 'http://localhost:8080', 'pnpm push:main');
   }
 
-  if(database === 'submissions') {
-    await migrateTables(database, "http://localhost:8081", 'pnpm push:submissions');
+  if (database === 'submissions') {
+    await migrateTables(
+      database,
+      'http://localhost:8081',
+      'pnpm push:submissions'
+    );
   }
 }
 
-async function migrateTables(database: string, databaseUrl: string, pushCommand: string) {
+async function migrateTables(
+  database: string,
+  databaseUrl: string,
+  pushCommand: string
+) {
   const packageDir = '../../../packages/db';
 
   await task('migrating tables', async s => {
@@ -27,7 +37,7 @@ async function migrateTables(database: string, databaseUrl: string, pushCommand:
           DATABASE_URL: databaseUrl,
           ...process.env
         },
-        cwd,
+        cwd
       });
       p.on('exit', code => {
         if (code === 0) {
@@ -36,7 +46,6 @@ async function migrateTables(database: string, databaseUrl: string, pushCommand:
           reject(code);
         }
       });
-
     });
     s.stop(`${database} database ready.`);
   });
