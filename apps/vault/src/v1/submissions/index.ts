79c02d40e72da8bd25d29d5c79ac61a2a3497547
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-30 12:46:30 +0100

    feat(apps/vault): add authentication for the protected routes

--- apps/vault/src/v1/submissions/index.ts
+++ apps/vault/src/v1/submissions/index.ts
@@ -1,20 +1,23 @@
+import {authentication} from '@/lib/middlewares/auth';
 import {pagination} from '@/lib/pagination';
 import {newRoute} from '@/lib/hono';
 
 import {registerGetSubmission} from './get';
 import {registerPutSubmission} from './put';
 import {registerPostSubmission} from './post';
-import {registerDeleteSubmission} from './delete';
 import {registerListSubmissions} from './list';
+import {registerDeleteSubmission} from './delete';
 
 const submissions = newRoute();
 const listSubmissions = newRoute();
 
+submissions.use(authentication());
 registerGetSubmission(submissions);
 registerPutSubmission(submissions);
 registerPostSubmission(submissions);
 registerDeleteSubmission(submissions);
 
+listSubmissions.use(authentication());
 listSubmissions.use(pagination());
 registerListSubmissions(listSubmissions);
 
