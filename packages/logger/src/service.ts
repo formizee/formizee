80cb0a536d41b533a47f28b80fe2913444770acb
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-30 01:27:55 +0100

    feat(packages): create the logger package

---
+++ packages/logger/src/service.ts
@@ -0,0 +1,72 @@
+import type {EdgeWithExecutionContext} from '@logtail/edge/dist/es6/edgeWithExecutionContext';
+import type {ExecutionContext} from '@cloudflare/workers-types';
+import {Log, type LogSchema} from './schema';
+import type {Fields, Logger} from './types';
+import {Logtail} from '@logtail/edge';
+
+export class ConsoleLogger implements Logger {
+  private readonly environment: LogSchema['environment'];
+  private readonly application: LogSchema['application'];
+  private readonly client: EdgeWithExecutionContext;
+  private readonly defaultFields: Fields;
+  private requestId: string;
+
+  constructor(opts: {
+    emitLogs: boolean;
+    requestId: string;
+    logtailToken: string;
+    ctx: ExecutionContext;
+    defaultFields?: Fields;
+    environment: LogSchema['environment'];
+    application: LogSchema['application'];
+  }) {
+    this.requestId = opts.requestId;
+    this.environment = opts.environment;
+    this.application = opts.application;
+    this.defaultFields = opts.defaultFields ?? {};
+
+    const baseLogger = new Logtail(opts.logtailToken, {
+      sendLogsToBetterStack: opts.emitLogs,
+      sendLogsToConsoleOutput: true,
+      ignoreExceptions: true
+    });
+    this.client = baseLogger.withExecutionContext(opts.ctx);
+  }
+
+  private marshal(
+    level: 'debug' | 'info' | 'warn' | 'error' | 'fatal',
+    message: string,
+    fields?: Fields
+  ): string {
+    return new Log({
+      type: 'log',
+      environment: this.environment,
+      application: this.application,
+      requestId: this.requestId,
+      time: Date.now(),
+      level,
+      message,
+      context: {...this.defaultFields, ...fields}
+    }).toString();
+  }
+
+  public debug(message: string, fields?: Fields): void {
+    this.client.debug(this.marshal('debug', message, fields));
+  }
+  public info(message: string, fields?: Fields): void {
+    this.client.info(this.marshal('info', message, fields));
+  }
+  public warn(message: string, fields?: Fields): void {
+    this.client.warn(this.marshal('warn', message, fields));
+  }
+  public error(message: string, fields?: Fields): void {
+    this.client.error(this.marshal('error', message, fields));
+  }
+  public fatal(message: string, fields?: Fields): void {
+    this.client.error(this.marshal('fatal', message, fields));
+  }
+
+  public setRequestId(requestId: string): void {
+    this.requestId = requestId;
+  }
+}
