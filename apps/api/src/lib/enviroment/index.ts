e09b045f6eaa79bc670225ad8176dc9816e513fe
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-11-30 16:36:29 +0100

    fix(apps/api): update the enviroment

--- apps/api/src/lib/enviroment/index.ts
+++ apps/api/src/lib/enviroment/index.ts
@@ -5,6 +5,7 @@ export const zEnv = z.object({
   // General
   ENVIROMENT: z.enum(['test', 'preview', 'development', 'production']),
   DASHBOARD_URL: z.string().url().default('http://localhost:3001'),
+  VAULT_URL: z.string().url().default('http://localhost:8888'),
   DOCS_URL: z.string().url().default('http://localhost:3002'),
   WEB_URL: z.string().url().default('http://localhost:3000'),
   API_URL: z.string().url().default('http://localhost:8787'),
@@ -12,22 +13,27 @@ export const zEnv = z.object({
 
   //Cloudflare Bindings
   cache: z.custom<KVNamespace>(),
-  keys: z.custom<KVNamespace>(),
 
   // Databases
   DATABASE_URL: z.string().url().default('http://localhost:8080'),
   DATABASE_AUTH_TOKEN: z.string().optional(),
 
   // Storage
-  STORAGE_SECRET_ACCESS_KEY: z.string().default('minio_root_password'),
-  STORAGE_ENDPOINT: z.string().url().default('http://localhost:3902'),
-  STORAGE_ACCESS_KEY_ID: z.string().default('minio_root_user'),
-  STORAGE_BUCKET: z.string().default('storage'),
   VAULT_SECRET: z.string().default(''),
 
   // Analytics
   TINYBIRD_URL: z.string().url().optional(),
   TINYBIRD_TOKEN: z.string().optional(),
+
+  // Logs
+  LOGTAIL_TOKEN: z.string().optional(),
+  EMIT_LOGS: z
+    .string()
+    .toLowerCase()
+    .transform(x => x === 'true')
+    .pipe(z.boolean()),
+
+  /* --- Secrets --- */
 
   // Communication
   RESEND_TOKEN: z.string().optional()
