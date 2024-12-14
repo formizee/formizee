5891b5ef01c61c1258bd4452e619e15dd1b32e07
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-12-15 00:12:49 +0100

    fix(apps/web): update the navbar style

--- apps/web/src/components/navbar/index.tsx
+++ apps/web/src/components/navbar/index.tsx
@@ -2,59 +2,56 @@
 
 import {Button, Logo, cn} from '@formizee/ui';
 import Link from 'next/link';
-import {usePathname} from 'next/navigation';
-import {useEffect, useState} from 'react';
-
-function NavbarLogo(): JSX.Element {
-  const pathname = usePathname();
-
-  if (pathname === '/') {
-    return <Logo />;
-  }
-
-  return (
-    <Link href="/">
-      <Logo />
-    </Link>
-  );
-}
 
 interface NavbarProps {
   className?: string;
 }
 
+const items = [
+  {
+    name: 'About',
+    href: '/about'
+  },
+  {
+    name: 'Pricing',
+    href: '/pricing'
+  },
+  {
+    name: 'Blog',
+    href: '/blog'
+  },
+  {
+    name: 'Docs',
+    href: 'https://docs.formizee.com'
+  }
+];
+
 export function Navbar(props: NavbarProps): JSX.Element {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  const handleScroll = (): void => {
-    const position = window.scrollY;
-    setScrollPosition(position);
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, {passive: true});
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  const background =
-    scrollPosition > 10
-      ? 'bg-black/70 border-b-neutral-800 backdrop-blur-md'
-      : 'bg-transparent border-b-transparent backdrop-blur-none';
-
   return (
     <nav
       className={cn(
         props.className,
-        'fixed top-0 z-50 flex h-14 w-full items-center justify-between border-b px-4 transition-all duration-500',
-        background
+        'fixed top-0 z-50 backdrop-blur-md bg-white/70 dark:bg-black/70 flex h-16 w-full items-center justify-between border-b px-4'
       )}
     >
-      <NavbarLogo />
-      <Button asChild variant="outline">
-        <Link href="/login">Login</Link>
+      <Link href="/">
+        <Logo />
+      </Link>
+      <ul className="flex flex-row gap-8">
+        {items.map(({name, href}) => (
+          <li
+            key={href}
+            className="transition-colors text-sm font-medium font-secondary text-neutral-700 hover:text-neutral-950 underline-offset-4 hover:underline"
+          >
+            <Link href={href}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+      <Button
+        asChild
+        className="max-w-32 font-secondary border-2 hover:border-neutral-500 border-neutral-700 bg-neutral-900"
+      >
+        <Link href="https://dashboard.formizee.com">Dashboard</Link>
       </Button>
     </nav>
   );
