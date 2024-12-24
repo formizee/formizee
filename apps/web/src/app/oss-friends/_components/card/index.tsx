73da8acba98e55b6cd43d010e95661991c145ef8
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-12-24 12:43:52 +0100

    fix(apps/web): update oss-friends card style

--- apps/web/src/app/oss-friends/_components/card/index.tsx
+++ apps/web/src/app/oss-friends/_components/card/index.tsx
@@ -10,7 +10,12 @@ export const ProductCard = (props: Props) => {
   const faviconHref = new URL(props.href).host;
 
   return (
-    <a className="group" href={props.href} target="_blank" rel="noreferrer">
+    <a
+      className="transition-all group"
+      href={props.href}
+      target="_blank"
+      rel="noreferrer"
+    >
       <article className="flex flex-col h-32 max-w-96 rounded-md border dark:border-neutral-800 gap-4 p-4 group-hover:shadow-md">
         <h3 className="flex flex-row gap-2 items-center font-secondary text-xl font-bold">
           <img
@@ -21,7 +26,7 @@ export const ProductCard = (props: Props) => {
             src={`https://icons.duckduckgo.com/ip3/${faviconHref}.ico`}
           />
           {props.name}
-          <LinkIcon className="transition-all text-neutral-400 dark:text-neutral-600 group-hover:text-neutral-950 dark:group-hover:text-neutral-50 group-hover:mb-1" />
+          <LinkIcon className="text-neutral-400 dark:text-neutral-600 group-hover:text-neutral-950 dark:group-hover:text-neutral-50 group-hover:mb-1" />
         </h3>
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
           {props.children}
