a31d657660aeec81fcd3548d9fe8be99f500a09d
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-12-24 12:43:00 +0100

    feat(internal/ui): add more languages to the code-block component

--- internal/ui/src/components/code-block/index.tsx
+++ internal/ui/src/components/code-block/index.tsx
@@ -1,134 +1,56 @@
-/* eslint-disable react/no-array-index-key -- There's is not going to be any sorting / addition / removal */
-import type {PrismTheme} from 'prism-react-renderer';
-import {Highlight} from 'prism-react-renderer';
+import {Highlight, Prism} from 'prism-react-renderer';
+import {getTheme} from './themes';
+import {cn} from '@formizee/ui';
 
-export type LanguageType = 'html' | 'js' | 'jsx' | 'ts' | 'tsx';
+(typeof global !== 'undefined' ? global : window).Prism = Prism;
+require('prismjs/components/prism-bash');
 
 interface CodeBlockProps {
+  color: string;
   children: string;
-  language: LanguageType;
+  language: string;
+  className?: string;
+  theme?: 'light' | 'dark';
 }
 
-const theme: PrismTheme = {
-  plain: {
-    cursor: '#fbbf24',
-    color: '#ffffff'
-  },
-  styles: [
-    {
-      types: ['comment', 'prolog', 'doctype', 'cdata'],
-      style: {
-        color: '#78716c'
-      }
-    },
-    {
-      types: ['namespace'],
-      style: {
-        opacity: 0.7
-      }
-    },
-    {
-      types: ['tag', 'operator', 'number'],
-      style: {
-        color: '#78716c'
-      }
-    },
-    {
-      types: ['property', 'function'],
-      style: {
-        color: '#9a86fd'
-      }
-    },
-    {
-      types: ['tag-id', 'selector', 'atrule-id'],
-      style: {
-        color: '#eeebff'
-      }
-    },
-    {
-      types: ['attr-name'],
-      style: {
-        color: '#a3a3a3'
-      }
-    },
-    {
-      types: [
-        'boolean',
-        'string',
-        'entity',
-        'url',
-        'attr-value',
-        'keyword',
-        'control',
-        'directive',
-        'unit',
-        'statement',
-        'regex',
-        'atrule',
-        'placeholder',
-        'variable'
-      ],
-      style: {
-        color: '#fbbf24'
-      }
-    },
-    {
-      types: ['deleted'],
-      style: {
-        textDecorationLine: 'line-through'
-      }
-    },
-    {
-      types: ['inserted'],
-      style: {
-        textDecorationLine: 'underline'
-      }
-    },
-    {
-      types: ['italic'],
-      style: {
-        fontStyle: 'italic'
-      }
-    },
-    {
-      types: ['important', 'bold'],
-      style: {
-        fontWeight: 'bold'
-      }
-    },
-    {
-      types: ['important'],
-      style: {
-        color: '#c4b9fe'
-      }
-    }
-  ]
-};
-
 export function CodeBlock(props: CodeBlockProps): JSX.Element {
+  const theme = getTheme(props.color, props.theme);
+
   return (
-    <Highlight code={props.children} language={props.language} theme={theme}>
-      {({tokens, getLineProps, getTokenProps}) => (
-        <pre className="flex flex-col md:text-lg" style={{}}>
-          {tokens.map((line, i) => (
-            <div
-              className="my-1 md:my-0.5"
-              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-              key={i}
-              style={getLineProps({line}).style}
-            >
-              <span className="mr-3 select-none text-sm text-neutral-400">
-                {i + 1}
-              </span>
-              {line.map((token, key) => (
-                // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-                <span key={key} {...getTokenProps({token})} />
-              ))}
-            </div>
-          ))}
-        </pre>
+    <div
+      className={cn(
+        props.className,
+        'animate-in fade-in slide-in-from-bottom-4 ease-in-out duration-1000'
       )}
-    </Highlight>
+    >
+      <Highlight
+        prism={Prism}
+        code={props.children}
+        language={props.language}
+        theme={theme}
+      >
+        {({tokens, getLineProps, getTokenProps}) => (
+          <pre className="flex flex-col" style={{}}>
+            {tokens.map((line, i) => (
+              <div
+                className="my-[1px] text-sm"
+                // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                key={i}
+                style={getLineProps({line}).style}
+              >
+                <span className="mr-3 select-none text-sm text-neutral-400">
+                  {i + 1}
+                </span>
+                {line.map((token, key) => (
+                  // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                  <span key={key} {...getTokenProps({token})} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    </div>
   );
 }
 
