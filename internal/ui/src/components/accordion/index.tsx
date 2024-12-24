79b683636011d9aa307a0cfdd6fa39bfe5e3c954
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-12-24 12:43:22 +0100

    fix(internal/ui): update accordion styles

--- internal/ui/src/components/accordion/index.tsx
+++ internal/ui/src/components/accordion/index.tsx
@@ -13,7 +13,7 @@ const AccordionItem = React.forwardRef<
 >(({className, ...props}, ref) => (
   <AccordionPrimitive.Item
     ref={ref}
-    className={cn('border-b', className)}
+    className={cn('border-b dark:border-neutral-800', className)}
     {...props}
   />
 ));
@@ -27,7 +27,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        'flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all hover:underline text-left [&[data-state=open]>svg]:rotate-180',
+        'flex flex-1 items-center justify-between py-4 font-medium transition-all text-left [&[data-state=open]>svg]:rotate-180',
         className
       )}
       {...props}
@@ -45,7 +45,7 @@ const AccordionContent = React.forwardRef<
 >(({className, children, ...props}, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="overflow-hidden text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    className="overflow-hidden leading-relaxed text-neutral-600 dark:text-neutral-400 data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
     <div className={cn('pb-4 pt-0', className)}>{children}</div>
