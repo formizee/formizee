427a3d11b32531affe680c3bb2c146e09c1864e2
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-12-25 17:27:10 +0100

    fix(apps/web): update tailwind.config.ts

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -12,9 +12,18 @@ const config: Pick<Config, 'presets'> = {
           },
           animation: {
             'border-beam':
-              'border-beam calc(var(--duration)*1s) infinite linear'
+              'border-beam calc(var(--duration)*1s) infinite linear',
+            'shiny-text': 'shiny-text 8s infinite'
           },
           keyframes: {
+            'shiny-text': {
+              '0%, 90%, 100%': {
+                'background-position': 'calc(-100% - var(--shiny-width)) 0'
+              },
+              '30%, 60%': {
+                'background-position': 'calc(100% + var(--shiny-width)) 0'
+              }
+            },
             'border-beam': {
               '100%': {
                 'offset-distance': '100%'
