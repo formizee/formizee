c03c26f00b9da1b30c6ecc831401ec25cec8d42f
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-12-27 18:22:58 +0100

    feat(apps/web): add the endpoint page

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/f/disabled',
+        destination: '/endpoint?type=disabled'
+      },
+      {
+        source: '/f/thanks-you',
+        destination: '/endpoint?type=thanks-you'
+      },
+      {
+        source: '/f/not-found',
+        destination: '/endpoint?type=not-found'
+      },
+      {
+        source: '/f/bad-request',
+        destination: '/endpoint?type=bad-request'
+      }
+    ];
+  }
+};
 
 export default nextConfig;
