6d40f4c4e8dd149ce4f1fb37f59e7e5d4171714c
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2025-01-08 17:09:36 +0100

    feat(apps/web): add the vercel analytics integration

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import {Analytics} from '@vercel/analytics/react';
 import {ThemeProvider} from '@/components/theme';
 import {Space_Grotesk} from 'next/font/google';
 import {Inter} from 'next/font/google';
@@ -51,6 +52,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
