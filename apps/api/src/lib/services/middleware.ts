38170d4871436c24a7dab3b787eb0720d7f67faf
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2025-01-09 11:10:36 +0100

    fix(apps/api): disable service bindings on development

--- apps/api/src/lib/services/middleware.ts
+++ apps/api/src/lib/services/middleware.ts
@@ -59,7 +59,7 @@ export function services(): MiddlewareHandler<HonoEnv> {
     const vault = new Vault({
       url: c.env.VAULT_URL,
       token: c.env.VAULT_SECRET,
-      serviceBinding: c.env.vault
+      serviceBinding: c.env.ENVIROMENT === 'development' ? undefined : c.env.vault
     });
 
     const email = new Resend(
