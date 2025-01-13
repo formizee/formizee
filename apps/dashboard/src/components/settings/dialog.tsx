e036a787a204140b8d35bcb1527119e2a684eff2
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2025-01-13 19:13:51 +0100

    fix(apps/dashboard): update the settings dialog accesibility

--- apps/dashboard/src/components/settings/dialog.tsx
+++ apps/dashboard/src/components/settings/dialog.tsx
@@ -1,9 +1,15 @@
 'use client';
 
-import {lazy, Suspense} from 'react';
-import {Dialog, DialogContent, DialogDescription} from '@formizee/ui';
 import {DialogLoading} from './loading';
+import {lazy, Suspense} from 'react';
 import {useSettings} from '.';
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogDescription
+} from '@formizee/ui';
 
 const Content = lazy(() => import('./content'));
 
@@ -13,7 +19,10 @@ export default function SettingsDialog() {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="flex overflow-hidden border-2 p-0 min-h-[700px] min-w-[500px] md:max-h-[700px] max-w-[500px] sm:max-w-[700px] lg:max-w-[900px]">
-        <DialogDescription className="hidden">Settings</DialogDescription>
+        <DialogTitle className="hidden">Settings</DialogTitle>
+        <DialogDescription className="hidden">
+          Here you can change the Formizee Settings
+        </DialogDescription>
         <Suspense fallback={<DialogLoading />}>
           <Content
             route={route}
