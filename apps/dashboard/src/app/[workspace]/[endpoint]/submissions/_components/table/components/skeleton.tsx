5b9625938a5360bd77d2254650d1d13899418cb1
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2025-01-17 18:31:12 +0100

    fix(apps/dashboard): update table to be more responsive on mobile

--- apps/dashboard/src/app/[workspace]/[endpoint]/submissions/_components/table/components/skeleton.tsx
+++ apps/dashboard/src/app/[workspace]/[endpoint]/submissions/_components/table/components/skeleton.tsx
@@ -5,8 +5,8 @@ export const LoadingSkeleton = () => (
   <Transition className="w-full h-full mt-4">
     <div className="flex gap-4">
       <Skeleton className="w-96 h-10" />
-      <Skeleton className="w-32 h-10" />
-      <Skeleton className="w-36 h-10" />
+      <Skeleton className="hidden sm:flex w-32 h-10" />
+      <Skeleton className="hidden sm:flex w-36 h-10" />
     </div>
     <Skeleton className="w-full h-96 mt-4" />
   </Transition>
