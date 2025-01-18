3c9423475a624c4c3635629790fb322dfff51fda
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2025-01-18 17:19:01 +0100

    fix(apps/web): update the sitemap according with the new site structure

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -5,8 +5,38 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://formizee.com',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'yearly',
       priority: 1
+    },
+    {
+      url: 'https://formizee.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8
+    },
+    {
+      url: 'https://formizee.com/oss-friends',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7
+    },
+    {
+      url: 'https://formizee.com/pricing',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7
+    },
+    {
+      url: 'https://formizee.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5
+    },
+    {
+      url: 'https://formizee.com/legal',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5
     }
   ];
 }
