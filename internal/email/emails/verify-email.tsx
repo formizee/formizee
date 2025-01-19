123df1bae435449be6aea49e33f443d868f86f76
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2025-01-19 12:12:41 +0100

    fix(internal/email): update the email template styles

--- internal/email/emails/verify-email.tsx
+++ internal/email/emails/verify-email.tsx
@@ -2,6 +2,7 @@ import {
   Body,
   Button,
   Container,
+  Font,
   Head,
   Heading,
   Hr,
@@ -20,10 +21,21 @@ interface EmailProps {
 export const AuthVerifyEmail = ({link}: EmailProps) => (
   <Tailwind config={config}>
     <Html lang="en">
-      <Head />
+      <Head>
+        <Font
+          fontFamily="Space-Grotesk"
+          fallbackFontFamily="sans-serif"
+          webFont={{
+            url: "https://www.formizee.com/_next/static/media/2d141e1a38819612-s.p.woff2",
+            format: "woff2",
+          }}
+          fontWeight={400}
+          fontStyle="normal"
+        />
+      </Head>
       <Body
         style={{fontFamily: 'Inter, System-UI, sans-serif'}}
-        className="bg-neutral-50 flex justify-center"
+        className="bg-white flex justify-center"
       >
         <Container className="max-w-[560px] m-2">
           <Img
@@ -32,43 +44,24 @@ export const AuthVerifyEmail = ({link}: EmailProps) => (
             className="rounded-xl mt-4 w-14 h-14"
           />
 
-          <Heading className="text-[22px] pt-4 pb-2 font-medium text-neutral-800">
+          <Heading style={{fontFamily: 'Inter, System-UI, sans-serif'}} className="text-[22px] pt-4 pb-0 font-medium text-neutral-950">
             Login Request for Formizee.
           </Heading>
-          <Text className="text-neutral-600 leading-[1.4] text-[15px]">
+          <Text className="text-neutral-600 leading-[1.4] text-[14px]">
             Click the button below to log into your workspaces
           </Text>
           <Button
-            className="flex w-24 px-3 border-neutral-300 py-2 justify-center items-center rounded-md my-8 text-neutral-700"
-            style={{border: '1px solid #d4d4d4'}}
+            className="flex w-20 px-3 bg-neutral-900 py-2 justify-center items-center rounded-md my-8 text-neutral-700"
             href={link}
+            style={{border: '2px solid #404040', color: '#fafafa'}}
           >
             <div className="flex gap-2">
-              <span className="font-medium text-sm">Sign In</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                width="18"
-                height="18"
-                fill="currentColor"
-                className="size-4"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M2 8a.75.75 0 0 1 .75-.75h8.69L8.22 4.03a.75.75 0 0 1 1.06-1.06l4.5 4.5a.75.75 0 0 1 0 1.06l-4.5 4.5a.75.75 0 0 1-1.06-1.06l3.22-3.22H2.75A.75.75 0 0 1 2 8Z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <span style={{fontFamily: 'Space-Grotesk', textWrap: 'nowrap'}} className="font-semibold text-sm">
+                Sign In
+              </span>
             </div>
           </Button>
-          <Text className="text-neutral-600 leading-[1.4] text-[15px]">
-            If you're having trouble with the button above,{' '}
-            <Link className="underline" href={link}>
-              click here
-            </Link>
-            .
-          </Text>
-          <Text className="text-neutral-600 leading-[1.4] text-[15px]">
+          <Text className="text-neutral-600 leading-[1.4] text-[14px]">
             Please do not share this link with anybody. If you didn't request
             this link, you can safely ignore this email.
           </Text>
